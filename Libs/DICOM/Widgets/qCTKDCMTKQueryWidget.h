d1796753410ba1dbd7a9715eefd39079ad2f95df
Steve Pieper <pieper@bwh.harvard.edu>
2010-03-10 11:51:29 -0500

    ENH: dicom widget works in process

---
+++ Libs/DICOM/Widgets/qCTKDCMTKQueryWidget.h
@@ -0,0 +1,25 @@
+#ifndef __qCTKDCMTKQueryWidget_h
+#define __qCTKDCMTKQueryWidget_h
+
+// QT includes 
+#include <QWidget>
+
+// qCTK includes
+#include <qCTKPimpl.h>
+
+#include "CTKDICOMWidgetsExport.h"
+
+class qCTKDCMTKQueryWidgetPrivate; 
+
+class Q_CTK_DICOM_WIDGETS_EXPORT qCTKDCMTKQueryWidget : public QWidget
+{
+public:
+  typedef QWidget Superclass;
+  explicit qCTKDCMTKQueryWidget(QWidget* parent=0);
+  virtual ~qCTKDCMTKQueryWidget();
+
+private:
+  QCTK_DECLARE_PRIVATE(qCTKDCMTKQueryWidget);
+};
+
+#endif
