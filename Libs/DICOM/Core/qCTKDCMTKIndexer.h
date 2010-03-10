b2dbd9d1c9cca2e3190dc919e8ae286b79f4d534
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2010-03-10 18:17:40 +0100

    ENH: first version of indexer, extended sql schema

---
+++ Libs/DICOM/Core/qCTKDCMTKIndexer.h
@@ -0,0 +1,24 @@
+#ifndef __qCTKDCMTKIndexer_h
+#define __qCTKDCMTKIndexer_h
+
+// QT includes 
+#include <QSqlDatabase>
+
+// qCTK includes
+#include <qCTKPimpl.h>
+
+#include "CTKDICOMCoreExport.h"
+
+class qCTKDCMTKIndexerPrivate;
+class Q_CTK_DICOM_CORE_EXPORT qCTKDCMTKIndexer  
+{
+public:
+  explicit qCTKDCMTKIndexer();
+  virtual ~qCTKDCMTKIndexer();
+  void AddDirectory(QSqlDatabase database, const QString& directoryName);
+
+private:
+  QCTK_DECLARE_PRIVATE(qCTKDCMTKIndexer);
+};
+
+#endif
