5d1f77297019c2af40014a970ec7b4ee5558fb2b
Steve Pieper <pieper@bwh.harvard.edu>
2010-03-10 11:04:06 -0500

    ENH: work-in-progress widgets for dicom viewer

---
+++ Libs/DICOM/Widgets/qCTKDCMTKServerNodeWidget.h
@@ -0,0 +1,25 @@
+#ifndef __qCTKDCMTKServerNodeWidget_h
+#define __qCTKDCMTKServerNodeWidget_h
+
+// QT includes 
+#include <QWidget>
+
+// qCTK includes
+#include <qCTKPimpl.h>
+
+#include "CTKDICOMWidgetsExport.h"
+
+class qCTKDCMTKServerNodeWidgetPrivate; 
+
+class Q_CTK_DICOM_WIDGETS_EXPORT qCTKDCMTKServerNodeWidget : public QWidget
+{
+public:
+  typedef QWidget Superclass;
+  explicit qCTKDCMTKServerNodeWidget(QWidget* parent=0);
+  virtual ~qCTKDCMTKServerNodeWidget();
+
+private:
+  QCTK_DECLARE_PRIVATE(qCTKDCMTKServerNodeWidget);
+};
+
+#endif
