26ab39c2939b6dab94140b69ec737d3e04f9f78a
Steve Pieper <pieper@bwh.harvard.edu>
2010-03-11 08:56:22 -0500

    ENH: use promotion to include designed widgets in designed form

--- Applications/ctkDICOM/ctkDICOM.cxx
+++ Applications/ctkDICOM/ctkDICOM.cxx
@@ -1,15 +1,20 @@
 //
 // QT includes
 #include <QApplication>
+#include <QFrame>
+#include <QHBoxLayout>
 
 // ctk includes
+#include "qCTKDCMTKQueryRetrieveWidget.h"
 #include "qCTKDCMTKQueryWidget.h"
 
 int main(int argc, char** argv)
 {
   QApplication app(argc, argv);
   
+  qCTKDCMTKQueryRetrieveWidget queryRetrieve;
   qCTKDCMTKQueryWidget query;
-  query.show();
+  //query.setParent(queryRetrieve.findChild<QFrame *>("queryFrame"));
+  queryRetrieve.show();
   return app.exec();
 }
