b2822276f0c08895c3ade38ec60ff2b2936ed274
Steve Pieper <pieper@bwh.harvard.edu>
2010-05-20 16:57:37 -0400

    ENH: add new subclass so first tab in tab widget can't be closed (no close box visible on tab)

---
+++ Libs/DICOM/Widgets/ctkDICOMQueryResultsTabWidget.cpp
@@ -0,0 +1,28 @@
+
+// Qt widgets
+#include <QDebug>
+#include <QTabBar>
+
+// ctkDICOMWidgets includes
+#include "ctkDICOMQueryResultsTabWidget.h"
+
+//----------------------------------------------------------------------------
+// ctkDICOMQueryResultsTabWidget methods
+
+//----------------------------------------------------------------------------
+ctkDICOMQueryResultsTabWidget::ctkDICOMQueryResultsTabWidget(QWidget* _parent):Superclass(_parent)
+{
+}
+
+//----------------------------------------------------------------------------
+ctkDICOMQueryResultsTabWidget::~ctkDICOMQueryResultsTabWidget()
+{
+}
+
+
+//----------------------------------------------------------------------------
+void ctkDICOMQueryResultsTabWidget::disableCloseOnTab(int index)
+{
+  this->tabBar()->setTabButton(0, QTabBar::RightSide, 0);
+  this->tabBar()->setTabButton(0, QTabBar::LeftSide, 0);
+}
---
+++ Libs/DICOM/Widgets/ctkDICOMQueryResultsTabWidget.h
@@ -0,0 +1,43 @@
+/*=========================================================================
+
+  Library:   CTK
+ 
+  Copyright (c) 2010  Kitware Inc.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.commontk.org/LICENSE
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ 
+=========================================================================*/
+
+#ifndef __ctkDICOMQueryResultsTabWidget_h
+#define __ctkDICOMQueryResultsTabWidget_h
+
+// Qt includes 
+#include <QWidget>
+#include <QTabWidget>
+
+
+#include "CTKDICOMWidgetsExport.h"
+
+class CTK_DICOM_WIDGETS_EXPORT ctkDICOMQueryResultsTabWidget : public QTabWidget
+{
+public:
+  typedef QTabWidget Superclass;
+  explicit ctkDICOMQueryResultsTabWidget(QWidget* parent=0);
+  virtual ~ctkDICOMQueryResultsTabWidget();
+
+  void disableCloseOnTab(int index);
+  
+private:
+};
+
+#endif
