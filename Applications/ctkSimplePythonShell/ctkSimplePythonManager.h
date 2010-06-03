6f80162b321a0707ca0b058f44d3b9856b9a9995
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2010-06-03 16:01:33 -0400

    ENH: Added first draft of the application ctkSimplePythonShell
    ctkSimplePythonShell is an attempt to demonstrate how easy it's possible
    to script an application using PythonQt and CTK.

---
+++ Applications/ctkSimplePythonShell/ctkSimplePythonManager.h
@@ -0,0 +1,27 @@
+#ifndef __ctkSimplePythonManager_h
+#define __ctkSimplePythonManager_h
+
+// CTK includes
+# include <ctkAbstractPythonManager.h>
+
+class PythonQtObjectPtr;
+
+class ctkSimplePythonManager : public ctkAbstractPythonManager
+{
+  Q_OBJECT
+
+public:
+  typedef ctkAbstractPythonManager Superclass;
+  ctkSimplePythonManager(QObject* parent=0);
+  ~ctkSimplePythonManager();
+  
+protected:
+
+  virtual QStringList pythonPaths();
+  virtual void preInitialization();
+
+};
+
+
+#endif
+
