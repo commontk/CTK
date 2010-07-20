1a014560602f9956d31640d8bf9c30b44f87cb8d
Julien Finet <julien.finet@kitware.com>
2010-07-20 15:20:07 -0400

    ENH: Add test for ctkDirectoryButton

---
+++ Libs/Widgets/Testing/Cpp/ctkDirectoryButtonTest1.cpp
@@ -0,0 +1,49 @@
+/*=========================================================================
+
+  Library:   CTK
+ 
+  Copyright (c) 2010  Kitware Inc.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.commontk.org/LICENSE
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ 
+=========================================================================*/
+
+// Qt includes
+#include <QApplication>
+#include <QTimer>
+
+// CTK includes
+#include "ctkDirectoryButton.h"
+
+// STD includes
+#include <cstdlib>
+#include <iostream>
+
+//-----------------------------------------------------------------------------
+int ctkDirectoryButtonTest1(int argc, char * argv [] )
+{
+  QApplication app(argc, argv);
+
+  ctkDirectoryButton ctkObject;
+  ctkObject.setOptions(QFileDialog::ShowDirsOnly | QFileDialog::ReadOnly);
+  ctkObject.show();
+
+  QTimer autoExit;
+  if (argc < 2 || QString(argv[1]) != "-I")
+    {
+    QObject::connect(&autoExit, SIGNAL(timeout()), &app, SLOT(quit()));
+    autoExit.start(100);
+    }
+  return app.exec();
+}
+
