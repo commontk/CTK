2bd8cbbd7de852f47802b5408e17a01416ec0da0
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2010-08-09 04:30:16 -0400

    ENH: ctkSimplePythonShell - CTKCore/CTKWidgets PythonQt wrapper are initialized within a custom PythonManager

--- Applications/ctkSimplePythonShell/ctkSimplePythonManager.cpp
+++ Applications/ctkSimplePythonShell/ctkSimplePythonManager.cpp
@@ -3,6 +3,13 @@
 #include "ctkSimplePythonManager.h"
 #include "ctkSimplePythonQtDecorators.h"
 
+// PythonQT includes
+#include <PythonQt.h>
+
+// PythonQt wrapper initialization methods
+void PythonQt_init_org_commontk_CTKCore(PyObject*);
+void PythonQt_init_org_commontk_CTKWidgets(PyObject*);
+
 //-----------------------------------------------------------------------------
 ctkSimplePythonManager::ctkSimplePythonManager(QObject* _parent) : Superclass(_parent)
 {
@@ -26,6 +33,10 @@ QStringList ctkSimplePythonManager::pythonPaths()
 void ctkSimplePythonManager::preInitialization()
 {
   Superclass::preInitialization();
+
+  // Initialize wrappers
+  PythonQt_init_org_commontk_CTKCore(0);
+  PythonQt_init_org_commontk_CTKWidgets(0);
 
   // Register decorators
   this->registerPythonQtDecorator(new ctkSimplePythonQtDecorators(this));  
