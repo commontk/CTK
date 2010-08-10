0377ce5c936c2761dad9371dd54c6561bfa643eb
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2010-08-10 00:21:48 -0400

    ENH: ctkSimplePythonShell - Conditionally include wrapper initialization code
    The file ctkSimplePythonShellConfigure.h.in is configure using the variable
    CTK_WRAP_PYTHONQT_{LIGHT, FULL}. Doing so allow to include the code responsible
    to initialize the wrapper only if it's required.

--- Applications/ctkSimplePythonShell/ctkSimplePythonManager.cpp
+++ Applications/ctkSimplePythonShell/ctkSimplePythonManager.cpp
@@ -1,14 +1,18 @@
 
+// PythonQT includes
+#include <PythonQt.h>
+
 // CTK includes
 #include "ctkSimplePythonManager.h"
 #include "ctkSimplePythonQtDecorators.h"
 
-// PythonQT includes
-#include <PythonQt.h>
+#include "ctkScriptingPythonCoreConfigure.h" // For CTK_WRAP_PYTHONQT_{LIGHT, FULL}
 
+#if defined(CTK_WRAP_PYTHONQT_LIGHT) || defined(CTK_WRAP_PYTHONQT_FULL)
 // PythonQt wrapper initialization methods
 void PythonQt_init_org_commontk_CTKCore(PyObject*);
 void PythonQt_init_org_commontk_CTKWidgets(PyObject*);
+#endif
 
 //-----------------------------------------------------------------------------
 ctkSimplePythonManager::ctkSimplePythonManager(QObject* _parent) : Superclass(_parent)
@@ -34,9 +38,11 @@ void ctkSimplePythonManager::preInitialization()
 {
   Superclass::preInitialization();
 
+#if defined(CTK_WRAP_PYTHONQT_LIGHT) || defined(CTK_WRAP_PYTHONQT_FULL)
   // Initialize wrappers
   PythonQt_init_org_commontk_CTKCore(0);
   PythonQt_init_org_commontk_CTKWidgets(0);
+#endif
 
   // Register decorators
   this->registerPythonQtDecorator(new ctkSimplePythonQtDecorators(this));  
