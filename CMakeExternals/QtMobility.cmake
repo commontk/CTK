dd6b7964a4aac713903ac677868b12593d2b5d53
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2010-08-26 18:48:06 +0200

    COMP: QtMobility: Fix the build system for MinGW. Installation of the dll and the header files is now properly done.

--- CMakeExternals/QtMobility.cmake
+++ CMakeExternals/QtMobility.cmake
@@ -40,20 +40,21 @@ IF(${add_project})
   
   SET(qtmobility_make_cmd)
   SET(qtmobility_install_cmd)
-  IF(UNIX OR MINGW)
+  IF(UNIX)
     SET(qtmobility_make_cmd make)
-	IF(MINGW AND NOT MSYS)
-	  SET(qtmobility_make_cmd mingw32-make)
-	ENDIF()
-	
     SET(qtmobility_config_args -${qtmobility_build_type} -libdir ${CTK_CMAKE_LIBRARY_OUTPUT_DIRECTORY} -no-docs -modules ${qtmobility_modules})
     SET(qtmobility_install_cmd ${qtmobility_make_cmd} install)
   ELSEIF(WIN32)
     SET(qtmobility_make_cmd nmake)
-    SET(qtmobility_win32_install_prefix ${ep_source_dir}/${proj}/install/)
-    FILE(TO_NATIVE_PATH ${qtmobility_win32_install_prefix} qtmobility_win32_native_install_prefix)
+    IF(MINGW AND NOT MSYS)
+      SET(qtmobility_make_cmd mingw32-make)
+    ENDIF()
+
+    SET(qtmobility_win32_install_prefix "${ep_source_dir}/${proj}/install/")
+    STRING(REPLACE "/" "\\" qtmobility_win32_native_install_prefix ${qtmobility_win32_install_prefix})
+
     SET(qtmobility_config_args -${qtmobility_build_type} -qt ${QT_BINARY_DIR} -prefix ${qtmobility_win32_native_install_prefix} -no-docs -modules ${qtmobility_modules})
-  
+
     CONFIGURE_FILE(${qtmobility_patch_dir}/QtMobility-1.0.0-install-win32.cmake.in
                    ${qtmobility_configured_patch_dir}/QtMobility-1.0.0-install-win32.cmake @ONLY)
 				   
