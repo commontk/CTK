371b04d7de04e5e4dd31094bb43be80444f9cce0
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2010-09-15 17:08:48 +0200

    FIX: host url was not created correctly for hosted apps

--- Plugins/org.commontk.dicom.examplehost/ctkDicomExampleHost.cpp
+++ Plugins/org.commontk.dicom.examplehost/ctkDicomExampleHost.cpp
@@ -10,16 +10,11 @@ void ctkDicomExampleHost::StartApplication(QString AppPath, const QUrl& App_URL)
 
     QStringList l;
     l.append("--hostURL");
-    l.append(QString("localhost:") + this->getPort() );
+    l.append(QString("http://localhost:") + QString::number(this->getPort()) );
     l.append("--applicationURL");
     l.append(App_URL.toString());
-    if (!QProcess::startDetached (
-            AppPath,l))
-    {
-        qCritical() << "application failed to start!";
-    }
+    qDebug() << "starting application: " << AppPath << " " << l;
 
-    this->appProcess.setProcessChannelMode(QProcess::ForwardedChannels);
     this->appProcess.start(AppPath,l);
 
 }
