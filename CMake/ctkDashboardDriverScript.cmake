e7516cdb40ffc24358e3b8db46d0b427c204b4b1
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2010-10-26 11:01:11 -0400

    Dashboard driver script now submit update info

--- CMake/ctkDashboardDriverScript.cmake
+++ CMake/ctkDashboardDriverScript.cmake
@@ -112,7 +112,6 @@ set(CTEST_UPDATE_COMMAND "${CTEST_GIT_COMMAND}")
 MACRO(run_ctest)
   ctest_start(${model})
   ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}" RETURN_VALUE res)
-  #ctest_submit(PARTS Update Notes)
 
   # force a build if this is the first run and the build dir is empty
   if(NOT EXISTS "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt")
@@ -132,6 +131,9 @@ ${ADDITIONNAL_CMAKECACHE_OPTION}
   endif()
   
   if (res GREATER 0 OR force_build)
+  
+    ctest_submit(PARTS Update)
+      
     message("Configure SuperBuild")
     
     set_property(GLOBAL PROPERTY SubProject SuperBuild)
