5778a254c37f46ef9b4e7d017b07257886554caa
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2010-11-25 23:29:59 -0500

    ctkSimplePythonShell: CTK wrapped library automatically initialized
    Script ctkSimplePythonShell defining convenient function like:
      app(), quit() or exit() is also loaded at startup

--- Applications/ctkSimplePythonShell/ctkSimplePythonShellMain.cpp
+++ Applications/ctkSimplePythonShell/ctkSimplePythonShellMain.cpp
@@ -1,15 +1,46 @@
 
 // Qt includes
 #include <QApplication>
+#include <QTextStream>
 
 // CTK includes
 #include <ctkPythonShell.h>
+#include <ctkCommandLineParser.h>
 
 #include "ctkSimplePythonManager.h"
 
 int main(int argc, char** argv)
 {
   QApplication app(argc, argv);
+
+  ctkCommandLineParser parser;
+  // Use Unix-style argument names
+  parser.setArgumentPrefix("--", "-");
+
+  // Add command line argument names
+  parser.addArgument("help", "h", QVariant::Bool, "Print usage information and exit.");
+  parser.addArgument("interactive", "I", QVariant::Bool, "Enable interactive mode");
+
+  // Parse the command line arguments
+  bool ok = false;
+  QHash<QString, QVariant> parsedArgs = parser.parseArguments(QCoreApplication::arguments(), &ok);
+  if (!ok)
+  {
+    QTextStream(stderr, QIODevice::WriteOnly) << "Error parsing arguments: "
+                                              << parser.errorString() << "\n";
+    return EXIT_FAILURE;
+  }
+
+  // Show a help message
+  if (parsedArgs.contains("help") || parsedArgs.contains("h"))
+  {
+    QTextStream(stdout, QIODevice::WriteOnly) << "ctkSimplePythonShell\n"
+          << "Usage\n\n"
+          << "  ctkSimplePythonShell [options] [<path-to-python-script> ...]\n\n"
+          << "Options\n"
+          << parser.helpText();
+    return EXIT_SUCCESS;
+  }
   
   ctkSimplePythonManager pythonManager;
   
@@ -17,6 +48,15 @@ int main(int argc, char** argv)
   shell.setAttribute(Qt::WA_QuitOnClose, true);
   shell.resize(600, 280);
   shell.show();
+
+  shell.setProperty("isInteractive", parsedArgs.contains("interactive"));
+
+  pythonManager.addObjectToPythonMain("_ctkPythonShellInstance", &shell);
+
+  foreach(const QString& script, parser.unparsedArguments())
+    {
+    pythonManager.executeFile(script);
+    }
   
   return app.exec();
 }
