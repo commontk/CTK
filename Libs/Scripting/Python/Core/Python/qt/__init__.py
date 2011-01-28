d9047576e145f77c597064cb5d7bdc1b8611fea8
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-01-28 07:20:56 -0500

    Within ctk and qt python module, hide variable CTK_VERBOSE_IMPORT from user

--- Libs/Scripting/Python/Core/Python/qt/__init__.py
+++ Libs/Scripting/Python/Core/Python/qt/__init__.py
@@ -15,14 +15,14 @@ __kits_to_load = [
 ]
 
 # Set to True when debugging
-CTK_VERBOSE_IMPORT = False
+_CTK_VERBOSE_IMPORT = False
 
 for kit in __kits_to_load:
    try:
      exec "from PythonQt.Qt%s import *" % (kit)
    except ImportError as detail:
-     if CTK_VERBOSE_IMPORT:
+     if _CTK_VERBOSE_IMPORT:
        print detail
    
 # Removing things the user shouldn't have to see.
-del __kits_to_load
+del __kits_to_load, _CTK_VERBOSE_IMPORT
