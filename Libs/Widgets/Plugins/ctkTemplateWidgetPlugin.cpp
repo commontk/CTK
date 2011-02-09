2a22430d76dabaa055c54a32f4edd70c9ca63fff
Julien Finet <julien.finet@kitware.com>
2011-02-09 00:44:34 -0500

    Add file templates for a widget class

---
+++ Libs/Widgets/Plugins/ctkTemplateWidgetPlugin.cpp
@@ -0,0 +1,68 @@
+/*=========================================================================
+
+  Library:   CTK
+
+  Copyright (c) Kitware Inc.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.commontk.org/LICENSE
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+=========================================================================*/
+
+// CTK includes
+#include "ctkTemplateWidgetPlugin.h"
+#include "ctkTemplateWidget.h"
+
+//-----------------------------------------------------------------------------
+ctkTemplateWidgetPlugin::ctkTemplateWidgetPlugin(QObject* parentObject)
+  : QObject(parentObject)
+{
+}
+
+//-----------------------------------------------------------------------------
+QWidget* ctkTemplateWidgetPlugin::createWidget(QWidget* parentWidget)
+{
+  ctkTemplateWidget* newWidget = new ctkTemplateWidget(parentWidget);
+  return newWidget;
+}
+
+//-----------------------------------------------------------------------------
+QString ctkTemplateWidgetPlugin::domXml() const
+{
+  return "<widget class=\"ctkTemplateWidget\" \
+          name=\"TemplateWidget\">\n"
+          "</widget>\n";
+}
+
+//-----------------------------------------------------------------------------
+QIcon ctkTemplateWidgetPlugin::icon() const
+{
+  return QIcon(":/Icons/pushbutton.png");
+}
+
+//-----------------------------------------------------------------------------
+QString ctkTemplateWidgetPlugin::includeFile() const
+{
+  return "ctkTemplateWidget.h";
+}
+
+//-----------------------------------------------------------------------------
+bool ctkTemplateWidgetPlugin::isContainer() const
+{
+  return false;
+}
+
+//-----------------------------------------------------------------------------
+QString ctkTemplateWidgetPlugin::name() const
+{
+  return "ctkTemplateWidget";
+}
---
+++ Libs/Widgets/Testing/Cpp/ctkTemplateWidgetTest1.cpp
@@ -0,0 +1,56 @@
+/*=========================================================================
+
+  Library:   CTK
+
+  Copyright (c) Kitware Inc.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.commontk.org/LICENSE
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+=========================================================================*/
+
+// Qt includes
+#include <QApplication>
+#include <QSignalSpy>
+#include <QTimer>
+
+// CTK includes
+#include "ctkTemplateWidget.h"
+
+// STD includes
+#include <iostream>
+
+//-----------------------------------------------------------------------------
+int ctkTemplateWidgetTest1(int argc, char * argv [] )
+{
+  QApplication app(argc, argv);
+
+  ctkTemplateWidget templateWidget;
+  
+  QSignalSpy spy(&templateWidget, SIGNAL(propertyChanged()));
+  //do something to trigger the signal
+  if (spy.count() != 1)
+    {
+    std::cerr << "ctkTemplateWidget::setProperty(): "
+              << spy.count() << std::endl;
+    return EXIT_FAILURE;
+    }
+  spy.clear();
+
+  templateWidget.show();
+  if (argc < 2 || QString(argv[1]) != "-I" )
+    {
+    QTimer::singleShot(200, &app, SLOT(quit()));
+    }
+  return app.exec();
+}
+
