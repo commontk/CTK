15441f8afd23034a94b5daa95ea80d4811e75094
Julien Finet <julien.finet@kitware.com>
2011-02-09 18:46:19 -0500

    Cleanup ctkDICOMQueryRetrieveWidgetTest1
    Remove useless lines

--- Libs/DICOM/Widgets/Testing/Cpp/ctkDICOMQueryRetrieveWidgetTest1.cpp
+++ Libs/DICOM/Widgets/Testing/Cpp/ctkDICOMQueryRetrieveWidgetTest1.cpp
@@ -6,28 +6,13 @@
 // ctkDICOMCore includes
 #include "ctkDICOMQueryRetrieveWidget.h"
 
-// CTK includes
-//#include "ctkCheckableHeaderView.h"
-
 // STD includes
 #include <iostream>
-
-/* Test from build directory:
- ./CTK-build/bin/CTKDICOMCoreCxxTests ctkDICOMModelTest1 test.db ../CTK/Libs/DICOM/Core/Resources/dicom-sample.sql
-*/
 
 int ctkDICOMQueryRetrieveWidgetTest1( int argc, char * argv [] )
 {
   QApplication app(argc, argv);
-  /*
-  if (argc <= 2)
-    {
-    std::cerr << "Warning, no sql file given. Test stops" << std::endl;
-    std::cerr << "Usage: qctkDICOMModelTest1 <scratch.db> <dumpfile.sql>" << std::endl;
-    return EXIT_FAILURE;
-    }
-  */
-
+ 
   ctkDICOMQueryRetrieveWidget widget;
   widget.show();
 
