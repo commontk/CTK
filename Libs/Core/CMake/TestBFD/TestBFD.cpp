a7c6c0a90c51e17fbb2e71b2cee057c96b734aee
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-02-24 10:13:51 -0500

    Build an executable instead of a library as part of the HAVE_BFD test

--- Libs/Core/CMake/TestBFD/TestBFD.cpp
+++ Libs/Core/CMake/TestBFD/TestBFD.cpp
@@ -1,10 +1,16 @@
+
 #include <bfd.h>
 
-void TestBFD()
+// STD includes
+#include <cstdlib>
+
+int main(int /*argc*/, char * /*argv*/[])
 {
   bfd *abfd = 0;
   asymbol *symbol = 0;
   asection *p = 0;
 
   bfd_init();
+  
+  return EXIT_SUCCESS;
 }
