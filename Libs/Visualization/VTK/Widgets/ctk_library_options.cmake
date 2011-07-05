2214661cd6a554c995e40d840b54649a73943808
Julien Finet <julien.finet@kitware.com>
2011-07-05 18:49:14 -0400

    Expose USE_TRANSFER_FUNCTION_CHARTS
    Control wether the CTK VTK chart related classes can be compiled.
    It depends on the VTK version used.

---
+++ Libs/Visualization/VTK/Widgets/ctk_library_options.cmake
@@ -0,0 +1,10 @@
+#
+# See CMake/ctkMacroAddCtkLibraryOptions.cmake
+#
+# This file should list of options available for considered CTK library
+# For example: MYOPT1:OFF MYOPT2:ON
+#
+
+SET(ctk_library_options
+  USE_TRANSFER_FUNCTION_CHARTS:OFF
+  )
