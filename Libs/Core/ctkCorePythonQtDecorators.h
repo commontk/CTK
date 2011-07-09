56041faf4b577539f3f2b37548e63f512e038a2f
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-07-09 11:21:36 -0400

    Update license header associated with PythonQtDecorators

--- Libs/Core/ctkCorePythonQtDecorators.h
+++ Libs/Core/ctkCorePythonQtDecorators.h
@@ -1,14 +1,22 @@
-/*=auto=========================================================================
+/*=========================================================================
 
- Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) 
- All Rights Reserved.
+  Library:   CTK
 
- See Doc/copyright/copyright.txt
- or http://www.slicer.org/copyright/copyright.txt for details.
+  Copyright (c) Kitware Inc.
 
- Program:   3D Slicer
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
-=========================================================================auto=*/
+      http://www.commontk.org/LICENSE
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+=========================================================================*/
 
 #ifndef __ctkCorePythonQtDecorators_h
 #define __ctkCorePythonQtDecorators_h
