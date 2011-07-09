788f261e18b7237009820ff28f4d4a391820be0c
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2011-07-09 11:36:00 -0400

    Use URL to refer to ParaView license version 1.2

--- Libs/Scripting/Python/Widgets/ctkPythonConsole.cpp
+++ Libs/Scripting/Python/Widgets/ctkPythonConsole.cpp
@@ -27,7 +27,7 @@
    ParaView is a free software; you can redistribute it and/or modify it
    under the terms of the ParaView license version 1.2. 
 
-   See License_v1.2.txt for the full ParaView license.
+   See http://www.paraview.org/paraview/project/license.html for the full ParaView license.
    A copy of this license can be obtained by contacting
    Kitware Inc.
    28 Corporate Drive
