59dc280047ab2a686c2294c3bfa8f204024e7f6d
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2011-11-15 23:23:30 +0100

    Signals must now be published under specific topics using EventAdmin.

--- Plugins/org.commontk.eventadmin/dispatch/ctkEASignalPublisher_p.h
+++ Plugins/org.commontk.eventadmin/dispatch/ctkEASignalPublisher_p.h
@@ -40,7 +40,8 @@ public:
   QString getSignalName() const;
   QString getTopicName() const;
 
-protected Q_SLOTS:
+  QString getSignalName() const;
+  QString getTopicName() const;
 
   void publishSyncSignal(const ctkDictionary& eventProps);
   void publishAsyncSignal(const ctkDictionary& eventProps);
