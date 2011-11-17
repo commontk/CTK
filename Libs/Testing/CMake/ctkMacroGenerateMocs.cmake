9c52e67db9f422cd6463db0f05c865c683ca5524
Julien Finet <julien.finet@kitware.com>
2011-11-17 11:19:48 +0100

    Add QT4_GENERATE_MOCS macro to simplify test moc-ing

--- Libs/Testing/CMake/ctkMacroGenerateMocs.cmake
+++ Libs/Testing/CMake/ctkMacroGenerateMocs.cmake
@@ -5,6 +5,7 @@ MACRO (QT4_GENERATE_MOCS)
 
   FOREACH (fileIt ${ARGN})
     QT4_GENERATE_MOC( ${fileIt} moc_${fileIt})
-  ENDFOREACH(it)
+    MACRO_ADD_FILE_DEPENDENCIES(${fileIt} moc_${fileIt})
+  ENDFOREACH(fileIt)
 
 ENDMACRO (QT4_GENERATE_MOCS)
