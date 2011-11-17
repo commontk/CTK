0dfc8e399f2f48c6537a80461a6fbe5559b95350
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2011-11-17 18:27:13 +0100

    Restore external project qxmlrpc.

--- CMakeExternals/qxmlrpc.cmake
+++ CMakeExternals/qxmlrpc.cmake
@@ -2,7 +2,6 @@
 # qxmlrpc
 #
 
-set(qxmlrpc_DEPENDS)
 ctkMacroShouldAddExternalproject(qxmlrpc_LIBRARY add_project)
 if(${add_project})
 
@@ -15,40 +14,52 @@ if(${add_project})
   
   set(proj qxmlrpc)
   set(proj_DEPENDENCIES)
-  
-  set(qxmlrpc_DEPENDS ${proj})
 
-  if(NOT DEFINED qxmlrpc_DIR)
-    #message(STATUS "Adding project:${proj}")
-    
-    ExternalProject_Add(${proj}
-      SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
-      BINARY_DIR ${proj}-build
-      PREFIX ${proj}${ep_suffix}
-      GIT_REPOSITORY "${git_protocol}://github.com/commontk/qxmlrpc.git"
-      GIT_TAG "origin/patched"
-      CMAKE_GENERATOR ${gen}
-      INSTALL_COMMAND ""
-      CMAKE_ARGS
-        ${ep_common_args}
-        -DQT_QMAKE_EXECUTABLE:FILEPATH=${QT_QMAKE_EXECUTABLE}
-      DEPENDS
-        ${proj_DEPENDENCIES}
-      )
-    set(qxmlrpc_DIR "${CMAKE_BINARY_DIR}/${proj}-build")
-    
-    # Since qxmlrpc is statically build, there is not need to add its corresponding 
-    # library output directory to CTK_EXTERNAL_LIBRARY_DIRS
-  
-  else()
-    ctkMacroEmptyExternalproject(${proj} "${proj_DEPENDENCIES}")
-  endif()
-  
-  list(APPEND CTK_SUPERBUILD_EP_VARS qxmlrpc_DIR:PATH)
+  list(APPEND CTK_DEPENDENCIES ${proj})
 
   set(${qxmlrpc_enabling_variable}_LIBRARY_DIRS qxmlrpc_LIBRARY_DIRS)
   set(${qxmlrpc_enabling_variable}_INCLUDE_DIRS qxmlrpc_INCLUDE_DIRS)
   set(${qxmlrpc_enabling_variable}_FIND_PACKAGE_CMD qxmlrpc)
+  
+  if(CTK_SUPERBUILD)
+
+    if(NOT DEFINED qxmlrpc_DIR)
     
+      # Set CMake OSX variable to pass down the external project
+      set(CMAKE_OSX_EXTERNAL_PROJECT_ARGS)
+      if(APPLE)
+        list(APPEND CMAKE_OSX_EXTERNAL_PROJECT_ARGS
+          -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES}
+          -DCMAKE_OSX_SYSROOT=${CMAKE_OSX_SYSROOT}
+          -DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET})
+      endif()
+      
+      #message(STATUS "Adding project:${proj}")
+      ExternalProject_Add(${proj}
+        SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
+        BINARY_DIR ${proj}-build
+        PREFIX ${proj}${ep_suffix}
+        GIT_REPOSITORY "${git_protocol}://github.com/commontk/qxmlrpc.git"
+        GIT_TAG "origin/patched"
+        CMAKE_GENERATOR ${gen}
+        INSTALL_COMMAND ""
+        CMAKE_ARGS
+          ${ep_common_args}
+          -DQT_QMAKE_EXECUTABLE:FILEPATH=${QT_QMAKE_EXECUTABLE}
+        DEPENDS
+          ${proj_DEPENDENCIES}
+        )
+      set(qxmlrpc_DIR "${CMAKE_BINARY_DIR}/${proj}-build")
+      
+      # Since qxmlrpc is statically build, there is not need to add its corresponding 
+      # library output directory to CTK_EXTERNAL_LIBRARY_DIRS
+    
+    else()
+      ctkMacroEmptyExternalproject(${proj} "${proj_DEPENDENCIES}")
+    endif()
+    
+     list(APPEND CTK_SUPERBUILD_EP_VARS qxmlrpc_DIR:PATH)
+
+  endif()
 endif()
 
