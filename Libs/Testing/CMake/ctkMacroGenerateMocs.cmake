787c71b01414429c0c44d4d4a8b38f74cdcfb338
Julien Finet <julien.finet@kitware.com>
2011-11-17 10:09:38 +0100

    Add Testing utilities to do unit testing with QtTest framework
    Within the CTest framework, QTEST_MAIN can't be used as it defines a
    duplicated main() function. CTest requires the test function to have
    the same name as the test file.
    QTest::mouseMove() is not supporting moves whith a button down.
    We add a utility function ctkTest::mouseMove() to support the feature.
    It is surprising that such behavior doesn't exist by default in Qt. There
    might be some associated issues with it.

---
+++ Libs/Testing/CMake/ctkMacroGenerateMocs.cmake
@@ -0,0 +1,10 @@
+
+# QT4_GENERATE_MOCS(inputfiles ... )
+
+MACRO (QT4_GENERATE_MOCS)
+
+  FOREACH (fileIt ${ARGN})
+    QT4_GENERATE_MOC( ${fileIt} moc_${fileIt})
+  ENDFOREACH(it)
+
+ENDMACRO (QT4_GENERATE_MOCS)
