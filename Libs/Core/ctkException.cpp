08b6480b4e0693f2bbb8504ae88ac753bb0d1580
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2012-05-18 11:57:24 +0200

    Added a ctkException class.

---
+++ Libs/Core/ctkException.cpp
@@ -0,0 +1,125 @@
+/*=============================================================================
+  
+  Library: CTK
+  
+  Copyright (c) German Cancer Research Center,
+    Division of Medical and Biological Informatics
+    
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+  
+    http://www.apache.org/licenses/LICENSE-2.0
+    
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+  
+=============================================================================*/
+
+
+#include "ctkException.h"
+
+#include <typeinfo>
+
+#include <QDebug>
+
+// --------------------------------------------------------------------------
+ctkException::ctkException(const QString& msg)
+  : msg(msg), nestedException(0)
+{
+}
+
+// --------------------------------------------------------------------------
+ctkException::ctkException(const QString& msg, const ctkException& cause)
+  : msg(msg), nestedException(cause.clone())
+{
+}
+
+// --------------------------------------------------------------------------
+ctkException::ctkException(const ctkException& exc)
+  : std::exception(exc), msg(exc.msg)
+{
+  nestedException = exc.nestedException ? exc.nestedException->clone() : 0;
+}
+
+// --------------------------------------------------------------------------
+ctkException::~ctkException() throw()
+{
+  delete nestedException;
+}
+
+// --------------------------------------------------------------------------
+ctkException& ctkException::operator=(const ctkException& exc)
+{
+  if (&exc != this)
+  {
+    delete nestedException;
+    msg = exc.msg;
+    nestedException = exc.nestedException ? exc.nestedException->clone() : 0;
+  }
+  return *this;
+}
+
+// --------------------------------------------------------------------------
+const ctkException* ctkException::cause() const throw()
+{
+  return nestedException;
+}
+
+// --------------------------------------------------------------------------
+void ctkException::setCause(const ctkException& cause)
+{
+  delete nestedException;
+  nestedException = cause.clone();
+}
+
+// --------------------------------------------------------------------------
+const char *ctkException::name() const throw()
+{
+  return "ctkException";
+}
+
+// --------------------------------------------------------------------------
+const char* ctkException::className() const throw()
+{
+  return typeid(*this).name();
+}
+
+// --------------------------------------------------------------------------
+const char* ctkException::what() const throw()
+{
+  static std::string txt;
+  txt = std::string(name());
+  if (!msg.isEmpty())
+  {
+    txt += ": ";
+    txt += msg.toStdString();
+  }
+  if (nestedException)
+  {
+    txt +=  std::string("\n  Caused by: ") + nestedException->what();
+  }
+  return txt.c_str();
+}
+
+// --------------------------------------------------------------------------
+ctkException* ctkException::clone() const
+{
+  return new ctkException(*this);
+}
+
+// --------------------------------------------------------------------------
+void ctkException::rethrow() const
+{
+  throw *this;
+}
+
+//----------------------------------------------------------------------------
+QDebug operator<<(QDebug dbg, const ctkException& exc)
+{
+  dbg << exc.what();
+  return dbg.maybeSpace();
+}
