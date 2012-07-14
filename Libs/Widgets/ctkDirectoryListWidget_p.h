0ba80449b276560c51cc29b908b6aa94fec6548c
MattClarkson <m.clarkson@ucl.ac.uk>
2012-07-14 22:41:46 +0100

    Updated copyright info in ctkDirectoryListView and ctkDirectoryListWidget

--- Libs/Widgets/ctkDirectoryListWidget_p.h
+++ Libs/Widgets/ctkDirectoryListWidget_p.h
@@ -2,7 +2,7 @@
 
   Library:   CTK
 
-  Copyright (c) Kitware Inc.
+  Copyright (c) University College London.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
