74de8df3d32087d168177c47d5d7c6f95e574917
MattClarkson <m.clarkson@ucl.ac.uk>
2012-08-24 21:00:47 +0100

    Added doxygen for ctkCmdLineModuleDescription

--- Libs/CommandLineModules/Core/ctkCmdLineModuleDescription.h
+++ Libs/CommandLineModules/Core/ctkCmdLineModuleDescription.h
@@ -40,7 +40,8 @@ class ctkCmdLineModuleParameter;
  * \ingroup CommandLineModulesCore
  *
  * The parameters can be used for automated GUI generation or execution
- * of the module.
+ * of the module, and are directly related to the XML schema used to define
+ * a command line module.
  */
 class CTK_CMDLINEMODULECORE_EXPORT ctkCmdLineModuleDescription
 {
@@ -54,31 +55,75 @@ public:
 
   static ctkCmdLineModuleDescription parse(QIODevice* input);
 
+  /**
+   * @brief Returns the category, derived from the \code <category> \endcode tag.
+   */
   QString category() const;
 
+  /**
+   * @brief Returns the title, derived from the \code <title> \endcode tag.
+   */
   QString title() const;
 
+  /**
+   * @brief Returns the title, derived from the \code <description> \endcode tag.
+   */
   QString description() const;
 
+  /**
+   * @brief Returns the title, derived from the \code <version> \endcode tag.
+   */
   QString version() const;
 
+  /**
+   * @brief Returns the title, derived from the \code <documentation-url> \endcode tag.
+   */
   QString documentationURL() const;
 
+  /**
+   * @brief Returns the title, derived from the \code <license> \endcode tag.
+   */
   QString license() const;
 
+  /**
+   * @brief Returns the title, derived from the \code <acknowledgements> \endcode tag.
+   */
   QString acknowledgements() const;
 
+  /**
+   * @brief Returns the title, derived from the \code <contributor> \endcode tag.
+   */
   QString contributor() const;
 
+  /**
+   * @brief Should return a QIcon, but does not appear to be supported yet.
+   */
   QIcon logo() const;
 
+  /**
+   * \brief The XML can define groups of parameters, so this method returns
+   * a QList of ctkCmdLineModuleParameterGroup to handle groups.
+   */
   QList<ctkCmdLineModuleParameterGroup> parameterGroups() const;
 
+  /**
+   * @brief Searches the list of parameters, checking if a parameter has the given name.
+   * @param name the name of the parameter, derived from the \code <name> \endcode tag.
+   * @return true if this module has a parameter called name and false otherwise
+   */
   bool hasParameter(const QString& name) const;
 
+  /**
+   * @brief Returns the parameter specified by name
+   * @param name the name of the parameter, derived from the \code <name> \endcode tag.
+   * @return the parameter
+   * @throw ctkInvalidArgumentException if this module does not have this parameter.
+   */
   ctkCmdLineModuleParameter parameter(const QString& name) const;
 
-  // Does the module have any simple (primitive) return types?
+  /**
+   * @brief Does the module have any simple (primitive) return types?
+   */
   bool hasReturnParameters() const;
 
 private:
