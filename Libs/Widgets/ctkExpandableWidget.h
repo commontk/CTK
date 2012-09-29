dc09bfeb4bfb8520eaad6fd27a3f6f1d1b6b39d5
Julien Finet <julien.finet@kitware.com>
2012-09-29 18:33:38 -0400

    Further refine ctkSizeGrip/ctkExpandableWidget behavior
     - Set minimum size hint to allow shrinks.
     - Add comments
     - Double click resets the sizehint to defaults

--- Libs/Widgets/ctkExpandableWidget.h
+++ Libs/Widgets/ctkExpandableWidget.h
@@ -29,24 +29,47 @@ class QResizeEvent;
 #include "ctkWidgetsExport.h"
 class ctkExpandableWidgetPrivate;
 
-/**
- * \ingroup Widgets
- * \brief Frame that can be resized by the user.
-*/
+/// \ingroup Widgets
+/// \brief Widget that can be resized by the user using a corner size grip.
+/// ctkExpandableWidget is a container widget that has a user customizable
+/// sizeHint.
+/// \note If the widget fails to be resized, consider tweaking the size
+/// policy of the parents and children widgets.
+/// \sa ctkSizeGrip
 class CTK_WIDGETS_EXPORT ctkExpandableWidget: public QFrame
 {
   Q_OBJECT
-  // Qt::Horizontal|Qt::Vertical by default
+  /// This property controls the movement of freedom allowed to resize the widget.
+  /// The location of the size grip widget depends on the orientations:
+  ///  - bottom of the widget if Qt::Vertical
+  ///  - right of the widget if Qt::Horizontal
+  ///  - bottom right corner of the widget if Qt::Horizontal|Qt::Vertical.
+  /// Bottom right corner of the widget with a Qt::Horizontal|Qt::Vertical resize
+  /// movements of freedom by default.
+  /// \sa ctkSizeGrip::orientations, sizeGripInside, sizeGripMargins
   Q_PROPERTY(Qt::Orientations orientations READ orientations WRITE setOrientations)
-  // true by default.
+
+  /// This property controls whether the size grip widget overlays the children
+  /// widgets or it is moved into its own margin.
+  /// Please note that QWidget::setContentsMargins is controlled by
+  /// ctkExpandableWidget, any value set will be overwritten. You can still set
+  /// the layout contents margins though.
+  /// true by default.
+  /// \sa sizeGripMargins, orientations
   Q_PROPERTY(bool sizeGripInside READ isSizeGripInside WRITE setSizeGripInside)
+
+  /// This property controls the extra padding to give to the size grip widget.
+  /// Depending on the contents of ctkExpandableWidget, the location of the size
+  /// grip widget could look off and would benefit from being moved from a few
+  /// pixels.
+  /// When tweaking this property, you may want to make sure it works for all
+  /// styles and platforms.
+  /// \sa sizeGripInside, orientations
   Q_PROPERTY(QSize sizeGripMargins READ sizeGripMargins WRITE setSizeGripMargins)
+
 public:
   typedef QFrame Superclass;
 
-  /** Constructor
-   *  /param parent       Parent widget
-  */
   ctkExpandableWidget(QWidget *parent=0);
   virtual ~ctkExpandableWidget();
 
@@ -59,10 +82,12 @@ public:
   void setSizeGripMargins(QSize margins);
   QSize sizeGripMargins()const;
 
-  //virtual QSize minimumSizeHint()const;
+  virtual QSize minimumSizeHint()const;
   virtual QSize sizeHint()const;
 
 public Q_SLOTS:
+  /// Recompute the size hint of the widget and resize with regard to the
+  /// layout.
   void updateSizeHint();
 
 protected:
@@ -74,8 +99,6 @@ protected:
 private:
   Q_DECLARE_PRIVATE(ctkExpandableWidget);
   Q_DISABLE_COPY(ctkExpandableWidget);
-
-  //Q_PRIVATE_SLOT(d_ptr, void _q_recomputeCompleterPopupSize())
 };
 
 #endif // __ctkExpandableWidget_h
