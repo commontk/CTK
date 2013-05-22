7515049dbca6a0457eddd06eeaf6d9eea6370a15
Andreas Fetzer <a.fetzer@dkfz-heidelberg.de>
2013-05-22 09:32:45 -0400

    Created initial table view

---
+++ Libs/DICOM/Widgets/ctkDICOMTableView.h
@@ -0,0 +1,67 @@
+/*=========================================================================
+
+  Library:   CTK
+
+  Copyright (c) Kitware Inc.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0.txt
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+=========================================================================*/
+
+#ifndef CTKDICOMTABLEVIEW_H
+#define CTKDICOMTABLEVIEW_H
+
+#include "ctkDICOMWidgetsExport.h"
+
+// ctkDICOMCore includes
+#include "ctkDICOMDatabase.h"
+
+// Qt includes
+#include <QWidget>
+#include <QItemSelection>
+
+class ctkDICOMTableViewPrivate;
+
+/// \ingroup DICOM_Widgets
+
+class CTK_DICOM_WIDGETS_EXPORT ctkDICOMTableView : public QWidget
+{
+  Q_OBJECT
+
+public:
+  typedef QWidget Superclass;
+
+  explicit ctkDICOMTableView(QWidget* parent = 0);
+  ctkDICOMTableView (QWidget* parent, QSharedPointer<ctkDICOMDatabase> ctkDicomDataBase);
+  virtual ~ctkDICOMTableView();
+
+  void setCTKDicomDataBase(QSharedPointer<ctkDICOMDatabase> dicomDataBase);
+
+  // Settings for QTableView
+  void hideColumn(int column);
+
+public Q_SLOTS:
+  void onSelectionChanged(const QItemSelection &, const QItemSelection &);
+
+protected Q_SLOTS:
+  void onDatabaseChanged();
+
+protected:
+  QScopedPointer<ctkDICOMTableViewPrivate> d_ptr;
+
+private:
+  Q_DECLARE_PRIVATE(ctkDICOMTableView)
+  Q_DISABLE_COPY(ctkDICOMTableView)
+};
+
+#endif // CTKDICOMTABLEVIEW_H
