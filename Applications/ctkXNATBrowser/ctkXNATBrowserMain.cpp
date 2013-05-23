628b109c292722a50f8e9e2f2567bdaacea2e7bb
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2013-05-23 07:39:58 +0200

    Added initial ctkXNATBrowser application.

---
+++ Applications/ctkXNATBrowser/ctkXNATBrowserMain.cpp
@@ -0,0 +1,37 @@
+/*=============================================================================
+
+  Library: CTK
+
+  Copyright (c) German Cancer Research Center,
+    Division of Medical and Biological Informatics
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+=============================================================================*/
+
+// Qt includes
+#include <QApplication>
+
+#include "ctkXNATBrowserMainWindow.h"
+
+int main(int argc, char** argv)
+{
+  QApplication myApp(argc, argv);
+  myApp.setOrganizationName("CommonTK");
+  myApp.setApplicationName("XNATBrowser");
+
+  ctkXNATBrowserMainWindow mainWindow;
+  mainWindow.show();
+
+  return myApp.exec();
+}
