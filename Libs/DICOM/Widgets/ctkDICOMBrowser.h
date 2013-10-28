1465057013f40b8256f295f0b7eafd39ea576732
Andreas Fetzer <a.fetzer@dkfz-heidelberg.de>
2013-10-28 11:17:03 +0100

    Removed unused gui elements from dicom browser

--- Libs/DICOM/Widgets/ctkDICOMBrowser.h
+++ Libs/DICOM/Widgets/ctkDICOMBrowser.h
@@ -22,6 +22,7 @@
 #define __ctkDICOMBrowser_h
 
 // Qt includes 
+#include <QItemSelection>
 #include <QWidget>
 
 #include "ctkDICOMWidgetsExport.h"
@@ -37,7 +38,6 @@ class CTK_DICOM_WIDGETS_EXPORT ctkDICOMBrowser : public QWidget
   Q_OBJECT
   Q_PROPERTY(ctkDICOMDatabase* database READ database)
   Q_PROPERTY(QString databaseDirectory READ databaseDirectory WRITE setDatabaseDirectory)
-  Q_PROPERTY(bool searchWidgetPopUpMode READ searchWidgetPopUpMode WRITE setSearchWidgetPopUpMode)
   Q_PROPERTY(QStringList tagsToPrecache READ tagsToPrecache WRITE setTagsToPrecache)
   Q_PROPERTY(bool displayImportSummary READ displayImportSummary WRITE setDisplayImportSummary)
 
@@ -61,11 +61,6 @@ public:
   /// Also provides a dialog box for progress
   void updateDatabaseSchemaIfNeeded();
 
-  /// Setting search widget pop-up mode
-  /// Default value is false. Setting it to true will make
-  /// search widget to be displayed as pop-up widget
-  void setSearchWidgetPopUpMode(bool flag);
-  bool searchWidgetPopUpMode();
   ctkDICOMDatabase* database();
 
   /// Option to show or not import summary dialog.
@@ -87,10 +82,6 @@ public Q_SLOTS:
   void openExportDialog();
   void openQueryDialog();
   void onRemoveAction();
-
-  void suspendModel();
-  void resumeModel();
-  void resetModel();
 
   /// Import a directory - this is used when the user selects a directory
   /// from the Import Dialog, but can also be used externally to trigger
@@ -115,50 +106,10 @@ Q_SIGNALS:
 protected:
     QScopedPointer<ctkDICOMBrowserPrivate> d_ptr;
 protected Q_SLOTS:
-    void onModelSelected(const QModelIndex& index);
+    void onModelSelected(const QItemSelection&, const QItemSelection&);
 
-    /// To be called when a thumbnail in thumbnail list widget is selected
-    void onThumbnailSelected(const ctkThumbnailLabel& widget);
-
-    /// To be called when a thumbnail in thumbnail list widget is double-clicked
-    void onThumbnailDoubleClicked(const ctkThumbnailLabel& widget);
-
-    /// To be called when previous and next buttons are clicked
-    void onNextImage();
-    void onPreviousImage();
-    void onNextSeries();
-    void onPreviousSeries();
-    void onNextStudy();
-    void onPreviousStudy();
     /// To be called when dialog finishes
     void onQueryRetrieveFinished();
-
-    /// To be called when an entry of the tree list is collapsed
-    void onTreeCollapsed(const QModelIndex& index);
-
-    /// To be called when an entry of the tree list is expanded
-    void onTreeExpanded(const QModelIndex& index);
-
-    /// To be called when auto-play checkbox state changed
-    void onAutoPlayCheckboxStateChanged(int state);
-
-    /// Called by timer for auto-play functionality
-    void onAutoPlayTimer();
-
-    /// To be called when the value of thumbnail size slider bar is changed
-    void onThumbnailWidthSliderValueChanged(int val);
-
-    /// To be called when search parameters in query widget changed
-    void onSearchParameterChanged();
-
-    /// To be called after image preview displayed an image
-    void onImagePreviewDisplayed(int imageID, int count);
-
-private Q_SLOTS:
-
-    void onSearchPopUpButtonClicked();
-
-    void onSearchWidgetTopLevelChanged(bool topLevel);
 
 private:
   Q_DECLARE_PRIVATE(ctkDICOMBrowser);
