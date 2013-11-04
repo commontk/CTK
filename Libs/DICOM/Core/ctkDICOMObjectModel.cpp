13e0197762af570aeb3d9fa89ac66317d706b338
Alireza Mehrtash <mehrtash@bwh.harvard.edu>
2013-11-04 11:55:26 -0500

    copyright text modified

--- Libs/DICOM/Core/ctkDICOMObjectModel.cpp
+++ Libs/DICOM/Core/ctkDICOMObjectModel.cpp
@@ -2,8 +2,8 @@
 
   Library: CTK
 
-  Copyright (c) Brigham and Women's Hospital (BWH)
-  Copyright (c) University of Sheffield
+  Copyright (c) Brigham and Women's Hospital (BWH).
+  Copyright (c) University of Sheffield.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
--- Libs/DICOM/Widgets/ctkDICOMObjectListWidget.h
+++ Libs/DICOM/Widgets/ctkDICOMObjectListWidget.h
@@ -2,7 +2,7 @@
 
   Library:   CTK
 
-  Copyright (c)  Brigham and Women's Hospital (BWH)..
+  Copyright (c)  Brigham and Women's Hospital (BWH).
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
