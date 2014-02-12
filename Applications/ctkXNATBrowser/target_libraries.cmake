2b0556a212218b6df84e3a7171b32d70f6c101bd
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2014-02-12 21:18:44 +0100

    Removed obsolete ctkXNATBrowser application.

--- Applications/ctkXNATBrowser/target_libraries.cmake
+++
@@ -1,9 +0,0 @@
-#
-# See CMake/ctkFunctionGetTargetLibraries.cmake
-#
-# This file should list the libraries required to build the current CTK application.
-#
-
-set(target_libraries
-  CTKXNATWidgets
-  )
