130deabb440ebdab38ca877f6d4acef55c35c5a9
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2014-04-15 22:49:04 -0400

    Optimize QT4_GENERATE_MOCS macro getting moc flags only when needed
    See #449

--- CMake/ctkMacroGenerateMocs.cmake
+++ CMake/ctkMacroGenerateMocs.cmake
@@ -2,9 +2,10 @@
 # QT4_GENERATE_MOCS(inputfile1 [inputfile2 ...])
 
 macro(QT4_GENERATE_MOCS)
+  QT4_GET_MOC_FLAGS(_moc_flags)
+
   foreach(file ${ARGN})
     set(moc_file moc_${file})
-    QT4_GENERATE_MOC(${file} ${moc_file})
 
     get_filename_component(source_name ${file} NAME_WE)
     get_filename_component(source_ext ${file} EXT)
@@ -15,7 +16,13 @@ macro(QT4_GENERATE_MOCS)
         set(source_ext .cxx)
       endif()
     endif()
-    set_property(SOURCE ${source_name}${source_ext} APPEND PROPERTY OBJECT_DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${moc_file})
+
+    set(_cpp ${source_name}${source_ext})
+    set(_moc ${CMAKE_CURRENT_BINARY_DIR}/${moc_file})
+
+    QT4_CREATE_MOC_COMMAND(${_cpp} ${_moc} "${_moc_flags}" "")
+    MACRO_ADD_FILE_DEPENDENCIES(${_cpp} ${_moc})
+
   endforeach()
 endmacro()
 
