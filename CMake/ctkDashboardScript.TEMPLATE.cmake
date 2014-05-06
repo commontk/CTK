f73c51766f2cdb41d4991dc8900568b42b4a8299
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2014-05-05 20:29:00 -0400

    Update dashboard driver script to submit a regular project layout.
    As discussed during the CTK hackfest, instead of submitting submitting
    each CTK libraries, plugins, applications as a separate CDash subproject.
    This commit reverts to a "regular" CDash layout where the
    configure/build/test results are all presented on a single page. This will
    be easier to maintain and less confusing for the developer.
    
    Finally, the driver script is now downloaded directly from Github. This is
    now possible by using CMake version >= 2.8.9 having built-in support
    for https download. See [1]
    
    Fixes #457
    
    [1] http://cmake.org/gitweb?p=cmake.git;a=commit;h=4bcd84e65ad2f792c549989b9d773191ad75e5eb

--- CMake/ctkDashboardScript.TEMPLATE.cmake
+++ CMake/ctkDashboardScript.TEMPLATE.cmake
@@ -1,81 +1,126 @@
+####################################################################################
+# OS      :
+# Hardware:
+# GPU     :
+####################################################################################
+# WARNING - The specific version and processor type of this machine should be reported
+# in the header above. Indeed, this file will be send to the dashboard as a NOTE file.
 #
-# OS: Ubuntu 9.04 2.6.28-18-generic
-# Hardware: x86_64 GNU/Linux
-# GPU: NA
-#
+# On linux, you could run:
+#     'uname -o' and 'cat /etc/*-release' to obtain the OS name.
+#     'uname -mpi' to obtain hardware details.
+#     'glxinfo | grep OpenGL' to obtain GPU details.
+####################################################################################
 
-# Note: The specific version and processor type of this machine should be reported in the 
-# header above. Indeed, this file will be send to the dashboard as a NOTE file. 
-
-cmake_minimum_required(VERSION 2.8)
+cmake_minimum_required(VERSION 2.8.9)
 
 #
 # For additional information, see http://www.commontk.org/index.php/Dashboard_setup
 #
 
-#
-# Dashboard properties
-#
-set(MY_OPERATING_SYSTEM "Linux") # Windows, Linux, Darwin... 
-set(MY_COMPILER "g++4.3.3")
-set(MY_QT_VERSION "4.6.2")
-set(QT_QMAKE_EXECUTABLE "$ENV{HOME}/Projects/qtsdk-2010.02/qt/bin/qmake")
-set(CTEST_SITE "sagarmatha.kitware") # for example: mymachine.kitware, mymachine.dkfz, ...
-set(CTEST_DASHBOARD_ROOT "$ENV{HOME}/Projects/")
-set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+#-----------------------------------------------------------------------------
+# Experimental:
+#     - run_ctest() macro will be called *ONE* time
+#     - binary directory will *NOT* be cleaned
+# Continuous:
+#     - run_ctest() macro will be called EVERY 5 minutes ...
+#     - binary directory will *NOT* be cleaned
+#     - configure/build will be executed *ONLY* if the repository has been updated
+# Nightly:
+#     - run_ctest() macro will be called *ONE* time
+#     - binary directory *WILL BE* cleaned
+set(SCRIPT_MODE "Experimental") # "Experimental", "Continuous", "Nightly"
 
+# You could invoke the script with the following syntax:
+#  ctest -S karakoram_Slicer4_nightly.cmake -C <CTEST_BUILD_CONFIGURATION> -V
 #
+# Note that '-C <CTEST_BUILD_CONFIGURATION>' is mandatory on windows
+
+#-----------------------------------------------------------------------------
+# Dashboard properties
+#-----------------------------------------------------------------------------
+set(MY_OPERATING_SYSTEM "Ubuntu-13.10") # Windows, Linux, Darwin...
+set(MY_COMPILER "g++4.8.1")
+set(MY_QT_VERSION "4.8.5")
+set(QT_QMAKE_EXECUTABLE "$ENV{HOME}/Support/qt-everywhere-opensource-release-build-4.8.5/bin/qmake")
+set(CTEST_SITE "karakoram.kitware") # for example: mymachine.kitware, mymachine.dkfz, ...
+set(CTEST_DASHBOARD_ROOT "$ENV{HOME}/Dashboards/${SCRIPT_MODE}")
+
+# set(GIT_REPOSITORY http://github.com/YOURUSERNAME/CTK.git)
+set(GIT_TAG "") # origin/next
+
+# Each dashboard script should specify a unique ID per CTEST_DASHBOARD_ROOT.
+# It means the following directories will be created:
+#   <CTEST_DASHBOARD_ROOT>/<DIRECTORY_NAME>-<DIRECTORY_IDENTIFIER>        # Source directory
+#   <CTEST_DASHBOARD_ROOT>/<DIRECTORY_NAME>-<DIRECTORY_IDENTIFIER>-build  # Build directory
+set(DIRECTORY_IDENTIFIER  "0")
+
+# Open a shell and type in "cmake --help" to obtain the proper spelling of the generator
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(MY_BITNESS            "64")
+
+#-----------------------------------------------------------------------------
 # Dashboard options
-#
+#-----------------------------------------------------------------------------
 set(WITH_KWSTYLE FALSE)
 set(WITH_MEMCHECK FALSE)
-set(WITH_COVERAGE TRUE)
+set(WITH_COVERAGE FALSE)
 set(WITH_DOCUMENTATION FALSE)
 #set(DOCUMENTATION_ARCHIVES_OUTPUT_DIRECTORY ) # for example: $ENV{HOME}/Projects/Doxygen
 set(CTEST_BUILD_CONFIGURATION "Release")
 set(CTEST_TEST_TIMEOUT 500)
-set(CTEST_BUILD_FLAGS "-j4") # Use multiple CPU cores to build
+set(CTEST_BUILD_FLAGS "") # Use multiple CPU cores to build. For example "-j -l4" on unix
+set(CTEST_PARALLEL_LEVEL 8) # Number of tests running in parallel
 
-# experimental: 
-#     - run_ctest() macro will be called *ONE* time
-#     - binary directory will *NOT* be cleaned
-# continuous: 
-#     - run_ctest() macro will be called EVERY 5 minutes ... 
-#     - binary directory will *NOT* be cleaned
-#     - configure/build will be executed *ONLY* if the repository has been updated
-# nightly: 
-#     - run_ctest() macro will be called *ONE* time
-#     - binary directory *WILL BE* cleaned
-set(SCRIPT_MODE "experimental") # "experimental", "continuous", "nightly"
-
-#
-# Project specific properties
-#
-set(CTEST_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/CTK")
-set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/CTK-Superbuild-${CTEST_BUILD_CONFIGURATION}-${SCRIPT_MODE}")
-
-# Additionnal CMakeCache options - For example:
-# CTK_LIB_Widgets:BOOL=ON
-# CTK_APP_ctkDICOM:BOOL=ON
-
-set(ADDITIONNAL_CMAKECACHE_OPTION "
-#CTK_LIB_Scripting/Python/Widgets:BOOL=ON
+#-----------------------------------------------------------------------------
+# Additional CMakeCache options
+#-----------------------------------------------------------------------------
+set(ADDITIONAL_CMAKECACHE_OPTION "
 ")
 
+#-----------------------------------------------------------------------------
 # List of test that should be explicitly disabled on this machine
+#-----------------------------------------------------------------------------
 set(TEST_TO_EXCLUDE_REGEX "")
 
-# set any extra environment variables here
-set(ENV{DISPLAY} ":0")
+#-----------------------------------------------------------------------------
+# Set any extra environment variables here
+#-----------------------------------------------------------------------------
+if(UNIX)
+  set(ENV{DISPLAY} ":0")
+endif()
 
+#-----------------------------------------------------------------------------
+# Required executables
+#-----------------------------------------------------------------------------
+find_program(CTEST_GIT_COMMAND NAMES git)
 find_program(CTEST_COVERAGE_COMMAND NAMES gcov)
 find_program(CTEST_MEMORYCHECK_COMMAND NAMES valgrind)
-find_program(CTEST_GIT_COMMAND NAMES git)
 
-#
-# Git repository - Overwrite the default value provided by the driver script
-#
-# set(GIT_REPOSITORY http://github.com/YOURUSERNAME/CTK.git)
+#-----------------------------------------------------------------------------
+# Build Name
+#-----------------------------------------------------------------------------
+# Update the following variable to match the chosen build options. This variable is used to
+# generate both the build directory and the build name.
+# See http://my.cdash.org/CDash/index.php?project=CTK for examples
+set(BUILD_OPTIONS_STRING "${MY_BITNESS}bits-QT${MY_QT_VERSION}")
+
+#-----------------------------------------------------------------------------
+# Directory name
+#-----------------------------------------------------------------------------
+set(DIRECTORY_NAME "CTK")
+
+#-----------------------------------------------------------------------------
+# Build directory
+#-----------------------------------------------------------------------------
+set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/${DIRECTORY_NAME}-${DIRECTORY_IDENTIFIER}-build")
+file(WRITE "${CTEST_DASHBOARD_ROOT}/${DIRECTORY_NAME}-${DIRECTORY_IDENTIFIER}-build - ${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}-${SCRIPT_MODE}.txt" "Generated by ${CTEST_SCRIPT_NAME}")
+
+#-----------------------------------------------------------------------------
+# Source directory
+#-----------------------------------------------------------------------------
+set(CTEST_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/${DIRECTORY_NAME}-${DIRECTORY_IDENTIFIER}")
+
 
 ##########################################
 # WARNING: DO NOT EDIT BEYOND THIS POINT #
@@ -87,33 +132,38 @@ set(CTEST_NOTES_FILES "${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}")
 # Project specific properties
 #
 set(CTEST_PROJECT_NAME "CTK")
-set(CTEST_BUILD_NAME "${MY_OPERATING_SYSTEM}-${MY_COMPILER}-QT${MY_QT_VERSION}-${CTEST_BUILD_CONFIGURATION}")
+set(CTEST_BUILD_NAME "${MY_OPERATING_SYSTEM}-${MY_COMPILER}-${BUILD_OPTIONS_STRING}-${CTEST_BUILD_CONFIGURATION}")
 
 #
 # Display build info
 #
-message("site name: ${CTEST_SITE}")
-message("build name: ${CTEST_BUILD_NAME}")
-message("script mode: ${SCRIPT_MODE}")
-message("coverage: ${WITH_COVERAGE}, memcheck: ${WITH_MEMCHECK}")
+message("CTEST_SITE ................: ${CTEST_SITE}")
+message("CTEST_BUILD_NAME ..........: ${CTEST_BUILD_NAME}")
+message("SCRIPT_MODE ...............: ${SCRIPT_MODE}")
+message("CTEST_BUILD_CONFIGURATION .: ${CTEST_BUILD_CONFIGURATION}")
+message("WITH_KWSTYLE ..............: ${WITH_KWSTYLE}")
+message("WITH_COVERAGE: ............: ${WITH_COVERAGE}")
+message("WITH_MEMCHECK .............: ${WITH_MEMCHECK}")
+message("WITH_PACKAGES .............: ${WITH_PACKAGES}")
+message("WITH_DOCUMENTATION ........: ${WITH_DOCUMENTATION}")
+message("DOCUMENTATION_ARCHIVES_OUTPUT_DIRECTORY: ${DOCUMENTATION_ARCHIVES_OUTPUT_DIRECTORY}")
 
 #
-# Convenient macro allowing to download a file
+# Convenient function allowing to download a file
 #
-macro(downloadFile url dest)
+function(download_file url dest)
   file(DOWNLOAD ${url} ${dest} STATUS status)
   list(GET status 0 error_code)
   list(GET status 1 error_msg)
   if(error_code)
     message(FATAL_ERROR "error: Failed to download ${url} - ${error_msg}")
   endif()
-endmacro()
+endfunction()
 
 #
 # Download and include dashboard driver script 
 #
-set(url http://commontk.org/ctkDashboardDriverScript.cmake)
+set(url https://raw.githubusercontent.com/commontk/CTK/master/CMake/ctkDashboardDriverScript.cmake)
 set(dest ${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}.driver)
-downloadfile(${url} ${dest})
+download_file(${url} ${dest})
 include(${dest})
-
