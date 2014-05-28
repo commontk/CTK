commontk/CTK

commit 47b34216588caf1a6be6aeca93ef24c9bbaf248a

Add support for Qt5
Todo:
* Test with Qt4
* Make sure ctkMacroWrapPythonQt.cmake works correctly

CMake/ctkMacroWrapPythonQt.cmake
@@ -299,7 +299,9 @@ macro(ctkMacroWrapPythonQt WRAPPING_NAMESPACE TARGET SRCS_LIST_NAME SOURCES IS_W
   set(moc_flags)
 
   # Grab moc flags
-  QT4_GET_MOC_FLAGS(moc_flags)
+  if (CTK_QT_VERSION VERSION_EQUAL "4")
+    QT4_GET_MOC_FLAGS(moc_flags)
+  endif()
 
   # Prepare custom_command argument
   set(moc_flags_arg)

commit cddec776679c1815982402c4e423e288a51b6a23

Fixes configuration when enabling PythonQt wrapping
This commit fixes the incorrect Qt version check
introduced by commit 47b34216.

CMake/ctkMacroWrapPythonQt.cmake
@@ -163,9 +163,8 @@ macro(ctkMacroWrapPythonQt WRAPPING_NAMESPACE TARGET SRCS_LIST_NAME SOURCES IS_W
     COMMENT "PythonQt Wrapping - Generating ${wrapper_init_cpp_filename}"
     VERBATIM
     )
-
-  if (CTK_QT_VERSION VERSION_EQUAL "4")
-    QT5_WRAP_CPP(${TARGET}_MOC_CXX ${CMAKE_CURRENT_BINARY_DIR}/${wrapper_h_filename})
+  if(CTK_QT_VERSION VERSION_GREATER "4")
+    qt5_wrap_cpp(${TARGET}_MOC_CXX ${CMAKE_CURRENT_BINARY_DIR}/${wrapper_h_filename})
   else()
     QT4_WRAP_CPP(${TARGET}_MOC_CXX ${CMAKE_CURRENT_BINARY_DIR}/${wrapper_h_filename})
   endif()