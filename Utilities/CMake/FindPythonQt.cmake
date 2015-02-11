a3093f91d11089e72fc804df78c9f20ca1021240
Eric Heim <e.heim@dkfz-heidelberg.de>
2015-02-11 17:36:55 +0100

    Fixed python wrapping to use qt5 with PythonQT qt5 version from sourceforge

--- Utilities/CMake/FindPythonQt.cmake
+++ Utilities/CMake/FindPythonQt.cmake
@@ -12,7 +12,10 @@ endif()
 if(NOT EXISTS "${PYTHONQT_INSTALL_DIR}")
   find_path(PYTHONQT_INSTALL_DIR include/PythonQt/PythonQt.h DOC "Directory where PythonQt was installed.")
 endif()
-find_path(PYTHONQT_INCLUDE_DIR PythonQt.h "${PYTHONQT_INSTALL_DIR}/include/PythonQt" DOC "Path to the PythonQt include directory")
+find_path(PYTHONQT_INCLUDE_DIR PythonQt.h
+  PATHS "${PYTHONQT_INSTALL_DIR}/include/PythonQt"
+        "${PYTHONQT_INSTALL_DIR}/src"
+  DOC "Path to the PythonQt include directory")
 find_library(PYTHONQT_LIBRARY_RELEASE PythonQt PATHS "${PYTHONQT_INSTALL_DIR}/lib" DOC "The PythonQt library.")
 find_library(PYTHONQT_LIBRARY_DEBUG NAMES PythonQt${CTK_CMAKE_DEBUG_POSTFIX} PythonQt${CMAKE_DEBUG_POSTFIX} PythonQt PATHS "${PYTHONQT_INSTALL_DIR}/lib" DOC "The PythonQt library.")
 set(PYTHONQT_LIBRARY)
@@ -38,7 +41,7 @@ endif()
 set(PYTHONQT_FOUND 0)
 set(PythonQt_FOUND 0)
 if(PYTHONQT_INCLUDE_DIR AND PYTHONQT_LIBRARY)
-  # Currently CMake'ified PythonQt only supports building against a python Release build. 
+  # Currently CMake'ified PythonQt only supports building against a python Release build.
   # This applies independently of CTK build type (Release, Debug, ...)
   add_definitions(-DPYTHONQT_USE_RELEASE_PYTHON_FALLBACK)
   set(PYTHONQT_FOUND 1)
