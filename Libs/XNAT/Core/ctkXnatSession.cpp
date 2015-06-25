6c3ed1f94e32dc8dc41ae527ee95eb6d221c649a
Daniel Knorr <d.knorr@dkfz-heidelberg.de>
2015-06-25 16:59:24 +0200

    If the identifierPattern.exactMatch-CASE is used, the map has 2 entries (ID, content). Now the ID is set to a lately created object. content is unused.. (the last commit was a mistake.)

--- Libs/XNAT/Core/ctkXnatSession.cpp
+++ Libs/XNAT/Core/ctkXnatSession.cpp
@@ -573,10 +573,10 @@ void ctkXnatSession::save(ctkXnatObject* object)
   }
 
   const QList<QVariantMap>& maps = result->results();
-  if (maps.size() == 1 && maps[0].size() == 1)
+  if (maps.size() == 1 && maps[0].size() == 2)
   {
     QVariant id = maps[0][ctkXnatObject::ID];
-    if (id.isNull())
+    if (!id.isNull())
     {
       object->setId(id.toString());
     }
