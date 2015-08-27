a9612d0093b14fe50f24be9c94bf53dde719582b
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2015-08-27 12:10:02 -0400

    QtTesting: Update Git repository URL to fix invalid cert issues.
    Since QtTesting development has been moved to Github, this commit
    updates the URL accordingly.
    
    The error occurred when using git protocol:
    
      git clone https://paraview.org/QtTesting.git
      Cloning into 'QtTesting'...
      fatal: unable to access 'https://paraview.org/QtTesting.git/': Unknown SSL protocol error in connection to paraview.org:443
    
      git clone https://paraview.org/QtTesting.git
      Cloning into 'QtTesting'...
      fatal: unable to access 'https://paraview.org/QtTesting.git/': SSL certificate problem: unable to get local issuer certificate
    
    Reported-by: Sergey Aleshin <4memph@gmail.com>
    Thanks: Sergey Aleshin <4memph@gmail.com>

--- CMakeExternals/QtTesting.cmake
+++ CMakeExternals/QtTesting.cmake
@@ -36,7 +36,7 @@ if(NOT DEFINED QtTesting_DIR)
     set(location_args GIT_REPOSITORY ${${proj}_GIT_REPOSITORY}
                       GIT_TAG ${revision_tag})
   else()
-    set(location_args GIT_REPOSITORY "${git_protocol}://paraview.org/QtTesting.git"
+    set(location_args GIT_REPOSITORY "${git_protocol}://github.com/Kitware/QtTesting.git"
                       GIT_TAG ${revision_tag})
   endif()
 
