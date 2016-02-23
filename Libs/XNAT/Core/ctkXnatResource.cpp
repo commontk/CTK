43d7beeb4160ccc7e0fac2cab0931360ed7ec525
Andreas Fetzer <a.fetzer@dkfz-heidelberg.de>
2016-02-23 14:36:04 +0100

    Adding the xsiType=xnat:resource query parameter leads to error 422: Unprocessable entity
    This can be reproduced with curl.
    omitting this query parameter solve the issue

--- Libs/XNAT/Core/ctkXnatResource.cpp
+++ Libs/XNAT/Core/ctkXnatResource.cpp
@@ -173,7 +173,6 @@ void ctkXnatResource::downloadImpl(const QString& filename)
 void ctkXnatResource::saveImpl(bool /*overwrite*/)
 {
   ctkXnatSession::UrlParameters urlParams;
-  urlParams["xsiType"] = this->schemaType();
 
   const QMap<QString, QString>& properties = this->properties();
   QMapIterator<QString, QString> itProperties(properties);
