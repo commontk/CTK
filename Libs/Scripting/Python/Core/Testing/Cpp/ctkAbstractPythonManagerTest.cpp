64c1436fb51c9510cf66ea3920d267e7a685d15f
Mayeul Chassagnard <mayeul.chassagnard@kitware.com>
2016-09-26 13:56:08 -0400

    ENH: Tests for pythonAttributes added
    * Should return the attributes of an instantiate class
    For example: d.foo_class() should return the 'foo_instance_member'
    
    * Based on the prerequisite file in:
      /CTK/Libs/Scripting/Python/Core/Testing/Cpp/PythonAttributes-test.py

--- Libs/Scripting/Python/Core/Testing/Cpp/ctkAbstractPythonManagerTest.cpp
+++ Libs/Scripting/Python/Core/Testing/Cpp/ctkAbstractPythonManagerTest.cpp
@@ -54,7 +54,8 @@ private Q_SLOTS:
   void testExecuteFile();
   void testExecuteFile_data();
 
-  //void testPythonAttributes(); // TODO
+  void testPythonAttributes();
+  void testPythonAttributes_data();
 
   void testPythonModule();
   void testPythonModule_data();
@@ -268,6 +269,53 @@ void ctkAbstractPythonManagerTester::testExecuteFile_data()
 
   QTest::newRow("3-check __file__ attribute") << QString("print 'This file is: %s' % __file__")
                      << false;
+}
+
+// ----------------------------------------------------------------------------
+void ctkAbstractPythonManagerTester::testPythonAttributes()
+{
+  QFETCH(QString, pythonVariableName);
+  QFETCH(QStringList, expectedAttributeList);
+
+  QString test_path = __FILE__; // return the local path of this source file
+  // replace "ctkAbstractPythonManagerTest.py" by "PythonAttributes-test.py"
+  test_path.lastIndexOf("/");
+  test_path.replace(test_path.lastIndexOf("/"),
+                         test_path.size() - test_path.lastIndexOf("/"),
+                         "/PythonAttributes-test.py");
+  this->PythonManager.executeFile(test_path);
+
+  QStringList AttributeList = this->PythonManager.pythonAttributes(pythonVariableName, QString("__main__").toLatin1(), false);
+
+  foreach (const QString& expectedAttribute, expectedAttributeList)
+    {
+    QVERIFY(AttributeList.contains(expectedAttribute));
+    }
+}
+
+// ----------------------------------------------------------------------------
+void ctkAbstractPythonManagerTester::testPythonAttributes_data()
+{
+  QTest::addColumn<QString>("pythonVariableName");
+  QTest::addColumn<QStringList>("expectedAttributeList");
+
+  QTest::newRow("d.foo_class()")
+                     << "d.foo_class()"
+                     << (QStringList()
+                         << "FOO_CLASS_MEMBER"
+                         << "foo_class_method"
+                         << "foo_instance_member"
+                         << "foo_instance_method"
+                         << "instantiate_bar");
+
+  QTest::newRow("d.foo_class().instantiate_bar()")
+                     << "d.foo_class().instantiate_bar()"
+                     << (QStringList()
+                         << "BAR_CLASS_MEMBER"
+                         << "bar_class_method"
+                         << "bar_instance_member"
+                         << "bar_instance_method");
+
 }
 
 // ----------------------------------------------------------------------------
