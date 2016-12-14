commontk/CTK

commit 70ea952be2e1b5460f0cfcb192c88da128fc7da6

Set ctkPathLineEdit::nameFilters as property for use in designer and python

Libs/Widgets/ctkPathLineEdit.h
@@ -110,6 +110,11 @@ class CTK_WIDGETS_EXPORT ctkPathLineEdit: public QWidget
   /// If this property is set to a positive value, the minimumSizeHint() and sizeHint() take it into account.
   Q_PROPERTY(int minimumContentsLength READ minimumContentsLength WRITE setMinimumContentsLength)
 
+  /// This property holds the list of regular expressions (in wildcard mode) used to help the user
+  /// complete a line.
+  /// For example: "Images (*.jpg *.gif *.png)"
+  Q_PROPERTY(QStringList nameFilters READ nameFilters WRITE setNameFilters)
+
 public:
   enum Filter { Dirs        = 0x001,
                 Files       = 0x002,

commit 5db3f9320ed50b9d8179236cd3e84694dd7153ec

Fixes warnings.  * explicits type conversion.  * use of tr().  * removes duplicate Q_PROPERTY (regression from 70ea952)

Libs/Core/ctkBackTrace.cpp
@@ -170,7 +170,7 @@ int ctkBackTracePrivate::trace(void** array, size_t n) const
   }
 
   if (s_pfnCaptureStackBackTrace != 0) {
-    return s_pfnCaptureStackBackTrace(0, n, array, 0);
+    return s_pfnCaptureStackBackTrace(0, static_cast<ULONG>(n), array, 0);
   }
   return 0;
 }

Libs/Widgets/ctkPathLineEdit.cpp
@@ -116,7 +116,7 @@ void ctkPathLineEditPrivate::init()
   this->BrowseButton->setText("...");
   // Don't vertically stretch the path line edit unnecessary
   this->BrowseButton->setSizePolicy(QSizePolicy(QSizePolicy::Fixed, QSizePolicy::Ignored));
-  this->BrowseButton->setToolTip(q->tr("Open a dialog"));
+  this->BrowseButton->setToolTip(ctkPathLineEdit::tr("Open a dialog"));
 
   QObject::connect(this->BrowseButton,SIGNAL(clicked()),
                    q, SLOT(browse()));

Libs/Widgets/ctkPathLineEdit.h
@@ -64,7 +64,6 @@ class CTK_WIDGETS_EXPORT ctkPathLineEdit: public QWidget
   Q_FLAGS(Filters)
   Q_PROPERTY ( QString label READ label WRITE setLabel )
 
-  Q_PROPERTY ( QStringList nameFilters READ nameFilters WRITE setNameFilters)
   Q_PROPERTY ( Filters filters READ filters WRITE setFilters)
   Q_PROPERTY ( QString currentPath READ currentPath WRITE setCurrentPath USER true )
   /// Qt versions prior to 4.7.0 didn't expose QFileDialog::Options in the