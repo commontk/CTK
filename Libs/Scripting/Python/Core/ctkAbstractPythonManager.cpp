feeda1ecccb3f45d69426b5afce51a6dfa0e1bcc
Steve Pieper <pieper@isomics.com>
2017-01-10 23:35:24 -0500

    qt5: Scripting/Python/Core: Fix building of ctkAbstractPythonManager
    Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

--- Libs/Scripting/Python/Core/ctkAbstractPythonManager.cpp
+++ Libs/Scripting/Python/Core/ctkAbstractPythonManager.cpp
@@ -482,7 +482,7 @@ PyObject* ctkAbstractPythonManager::pythonObject(const QString& variableNameAndF
           continue;
           }
         QString keyStr = PyString_AsString(key);
-        if (keyStr.operator ==(compareFunction))
+        if (keyStr.operator ==(compareFunction.toLatin1()))
           {
           finalPythonObject = value;
           break;
@@ -509,7 +509,7 @@ PyObject* ctkAbstractPythonManager::pythonModule(const QString& module)
     }
   foreach(const QString& module, moduleList)
     {
-    object = PyDict_GetItemString(dict, module.toAscii().data());
+    object = PyDict_GetItemString(dict, module.toLatin1().data());
     if (prevObject)
       {
       Py_DECREF(prevObject);
