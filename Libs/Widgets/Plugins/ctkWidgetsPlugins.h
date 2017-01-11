f3ced8e8942b8dc8292aac2c5382dfe1dd9601fb
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-01-10 23:30:54 -0500

    designer plugin: Fix deprecated use of headers in Qt5
    /path/to/Qt5.5.1/5.5/gcc_64/include/QtDesigner/QDesignerCustomWidgetInterface:4:4: warning: #warning Header <QtDesigner/QDesignerCustomWidgetInterface> is deprecated. Please include <QtUiPlugin/QDesignerCustomWidgetInterface> instead. [-Wcpp]
     #  warning Header <QtDesigner/QDesignerCustomWidgetInterface> is deprecated. Please include <QtUiPlugin/QDesignerCustomWidgetInterface> instead.
        ^

--- Libs/Widgets/Plugins/ctkWidgetsPlugins.h
+++ Libs/Widgets/Plugins/ctkWidgetsPlugins.h
@@ -22,7 +22,12 @@
 #define __ctkWidgetsPlugins_h
 
 // Qt includes
+#include <QtGlobal>
+#if QT_VERSION < QT_VERSION_CHECK(5,0,0)
 #include <QDesignerCustomWidgetCollectionInterface>
+#else
+#include <QtUiPlugin/QDesignerCustomWidgetCollectionInterface>
+#endif
 
 // CTK includes
 #include "ctkWidgetsPluginsExport.h"
