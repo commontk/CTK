401177dc15df3c193079c6b8b782f57a95fdcf02
Dženan Zukić <dzenan.zukic@kitware.com>
2017-06-13 17:05:31 -0400

    COMP: removing duplicate else which offends CMake 3.9.0

--- CMake/ctkMacroSetupQt.cmake
+++ CMake/ctkMacroSetupQt.cmake
@@ -73,7 +73,6 @@ macro(ctkMacroSetupQt)
       message(FATAL_ERROR "error: Qt4 was not found on your system. You probably need to set the QT_QMAKE_EXECUTABLE variable")
     endif()
 
-  else()
     message(FATAL_ERROR "error: Qt4 was not found on your system. You probably need to set the QT_QMAKE_EXECUTABLE variable")
   endif()
 
