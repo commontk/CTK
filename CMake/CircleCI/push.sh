a461d18a25ac87b44774c78733d2a55c4c385abc
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-06-04 16:23:50 -0400

    ci: Remove obsolete script and dockerfiles

--- CMake/CircleCI/push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-die() {
-  echo "Error: $@" 1>&2
-  exit 1;
-}
-
-docker push commontk/ctk-test:opengl-qt4
-docker push commontk/ctk-test:opengl-qt5
--- CMake/CircleCI/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-set -o pipefail
-
-script_dir="`cd $(dirname $0); pwd`"
-
-$script_dir/run_opengl.sh -i commontk/ctk-test:opengl-$1 -c opengl-$1 -p 6081 -r --env="CIRCLE_SHA1=$2" -r --env="CIRCLE_BRANCH=$3" -r --env="SITE_BUILD_TYPE=$4"
