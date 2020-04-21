8ec913626970412a90003acc91e72469d051537d
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2020-04-20 22:05:54 -0400

    COMP: Fix Qt4 build adding fallback implementation for qUtf8Printable() (#909)

---
+++ Libs/Core/ctkCompatibility_p.h
@@ -0,0 +1,38 @@
+/*=========================================================================
+
+  Library:   CTK
+
+  Copyright (c) Kitware Inc.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0.txt
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+=========================================================================*/
+
+#ifndef __ctkCompatibility_p_h
+#define __ctkCompatibility_p_h
+
+//
+// This file is not part of the CTK API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include <QtGlobal>
+
+#if (QT_VERSION < QT_VERSION_CHECK(5, 4, 0))
+#define qUtf8Printable(string) QString(string).toUtf8().constData()
+#endif
+
+#endif
