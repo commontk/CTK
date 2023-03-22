commontk/CTK

commit c819ae7376b2c5677430ac18e430a16abfc9fa4b

BUG: Mark translatable strings in DICOM/Core
see Slicer/SlicerLanguagePacks#17
see Slicer/SlicerLanguagePacks#12

Libs/DICOM/Core/ctkDICOMDisplayedFieldGeneratorRadiotherapySeriesDescriptionRule.h
@@ -22,6 +22,7 @@
 #define __ctkDICOMDisplayedFieldGeneratorRadiotherapySeriesDescriptionRule_h
 
 // Qt includes
+#include <QCoreApplication.h>
 #include <QStringList>
 
 #include "ctkDICOMDisplayedFieldGeneratorAbstractRule.h"
@@ -31,6 +32,8 @@
 /// Special rule for generating series description displayed fields for different RT modalities
 class CTK_DICOM_CORE_EXPORT ctkDICOMDisplayedFieldGeneratorRadiotherapySeriesDescriptionRule : public ctkDICOMDisplayedFieldGeneratorAbstractRule
 {
+  Q_DECLARE_TR_FUNCTIONS(ctkDICOMDisplayedFieldGeneratorRadiotherapySeriesDescriptionRule)
+
 public:
   /// Constructor
   explicit ctkDICOMDisplayedFieldGeneratorRadiotherapySeriesDescriptionRule();

Libs/DICOM/Core/ctkDICOMItem.h
@@ -27,6 +27,7 @@
 
 #include <dcmtk/dcmdata/dcdatset.h> // DCMTK DcmDataset
 
+#include <QCoreApplication.h>
 #include <QtCore>
 
 class DcmDataDictionary;
@@ -65,7 +66,9 @@ typedef ctkDICOMItem ctkDICOMItem;
 
 class CTK_DICOM_CORE_EXPORT ctkDICOMItem
 {
-public:
+  Q_DECLARE_TR_FUNCTIONS(ctkDICOMItem)
+
+  public:
     typedef QObject Superclass;
     ///
     /// \brief Create an empty object. This has to be initialized by one of

Libs/DICOM/Core/ctkDICOMDisplayedFieldGeneratorRadiotherapySeriesDescriptionRule.cpp
@@ -22,9 +22,9 @@
 
 //------------------------------------------------------------------------------
 ctkDICOMDisplayedFieldGeneratorRadiotherapySeriesDescriptionRule::ctkDICOMDisplayedFieldGeneratorRadiotherapySeriesDescriptionRule()
-  : EmptySeriesDescriptionRtPlan("Unnamed RT Plan")
-  , EmptySeriesDescriptionRtStruct("Unnamed RT Structure Set")
-  , EmptySeriesDescriptionRtImage("Unnamed RT Image")
+  : EmptySeriesDescriptionRtPlan(tr("Unnamed RT Plan"))
+  , EmptySeriesDescriptionRtStruct(tr("Unnamed RT Structure Set"))
+  , EmptySeriesDescriptionRtImage(tr("Unnamed RT Image"))
 {
 }
 

Libs/DICOM/Core/ctkDICOMIndexer.cpp
@@ -102,7 +102,7 @@ void ctkDICOMIndexerPrivateWorker::start()
 
   do
   {
-    emit progressStep("Parsing DICOM files");
+    emit progressStep(ctkDICOMIndexer::tr("Parsing DICOM files"));
     emit progress(0);
     // Make a local copy to avoid the need of frequent locking
     this->RequestQueue->modifiedTimeForFilepath(this->ModifiedTimeForFilepath);
@@ -128,7 +128,7 @@ void ctkDICOMIndexerPrivateWorker::start()
     timeProbe.start();
 
     // Update displayed fields according to inserted DICOM datasets
-    emit progressStep("Updating database displayed fields");
+    emit progressStep(ctkDICOMIndexer::tr("Updating database displayed fields"));
     emit progress(this->TimePercentageIndexing);
 
     database.updateDisplayedFields();
@@ -149,7 +149,7 @@ void ctkDICOMIndexerPrivateWorker::start()
 
   this->RequestQueue->setIndexing(false);
   emit progress(100);
-  emit progressStep("Indexing complete");
+  emit progressStep(ctkDICOMIndexer::tr("Indexing complete"));
   emit indexingComplete(patientsCountAfter - patientsCountBefore, studiesCountAfter-studiesCountBefore,
     seriesCountAfter-seriesCountBefore, imagesCountAfter - imagesCountBefore);
 }
@@ -217,9 +217,9 @@ void ctkDICOMIndexerPrivateWorker::processIndexingRequest(DICOMIndexingQueue::In
       int resultsCount = this->RequestQueue->pushIndexingResult(indexingResult);
       if (resultsCount >= REQUEST_RESULTS_CACHE_MAXIMUM_SIZE)
       {
-        emit progressStep("Updating database fields");
+        emit progressStep(ctkDICOMIndexer::tr("Updating database fields"));
         this->writeIndexingResultsToDatabase(database);
-        emit progressStep("Parsing DICOM files");
+        emit progressStep(ctkDICOMIndexer::tr("Parsing DICOM files"));
       }
     }
     else
@@ -235,15 +235,18 @@ void ctkDICOMIndexerPrivateWorker::processIndexingRequest(DICOMIndexingQueue::In
 
   if (alreadyAddedFileCount > 0)
   {
-    logger.debug(QString("Skipped %1 files that were already in the database: %2...").arg(
-      alreadyAddedFileCount).arg(alreadyAddedFiles.join(", ")));
+    logger.debug(
+      QString("Skipped %1 files that were already in the database: %2...")
+      .arg(alreadyAddedFileCount)
+      .arg(alreadyAddedFiles.join(", "))
+    );
   }
 
   if (this->RequestQueue->isIndexingRequestsEmpty())
   {
-    emit progressStep("Updating database fields");
+    emit progressStep(ctkDICOMIndexer::tr("Updating database fields"));
     this->writeIndexingResultsToDatabase(database);
-    emit progressStep("Parsing DICOM files");
+    emit progressStep(ctkDICOMIndexer::tr("Parsing DICOM files"));
   }
 
   float elapsedTimeInSeconds = timeProbe.elapsed() / 1000.0;
@@ -560,7 +563,11 @@ bool ctkDICOMIndexer::addDicomdir(const QString& directoryName, bool copyFile/*=
       logger.debug( "Reading new Patient:" );
       if (patientRecord->findAndGetOFString(DCM_PatientName, patientsName).bad())
       {
-        logger.warn( "DICOMDIR file at "+directoryName+" is invalid: patient name not found. All records belonging to this patient will be ignored.");
+        logger.warn(
+          QString("DICOMDIR file at %1 is invalid: patient name not found. "
+             "All records belonging to this patient will be ignored.")
+          .arg(directoryName)
+        );
         success = false;
         continue;
       }
@@ -570,7 +577,12 @@ bool ctkDICOMIndexer::addDicomdir(const QString& directoryName, bool copyFile/*=
         logger.debug( "Reading new Study:" );
         if (studyRecord->findAndGetOFString(DCM_StudyInstanceUID, studyInstanceUID).bad())
         {
-          logger.warn( "DICOMDIR file at "+directoryName+" is invalid: study instance UID not found for patient "+ QString(patientsName.c_str())+". All records belonging to this study will be ignored.");
+          logger.warn(
+            QString("DICOMDIR file at %1 is invalid: study instance UID not found for patient %2. "
+               "All records belonging to this study will be ignored.")
+            .arg(directoryName)
+            .arg(patientsName.c_str())
+          );
           success = false;
           continue;
         }
@@ -581,7 +593,13 @@ bool ctkDICOMIndexer::addDicomdir(const QString& directoryName, bool copyFile/*=
           logger.debug( "Reading new Series:" );
           if (seriesRecord->findAndGetOFString(DCM_SeriesInstanceUID, seriesInstanceUID).bad())
           {
-            logger.warn( "DICOMDIR file at "+directoryName+" is invalid: series instance UID not found for patient "+ QString(patientsName.c_str())+", study "+ QString(studyInstanceUID.c_str())+". All records belonging to this series will be ignored.");
+            logger.warn(
+              QString("DICOMDIR file at %1 is invalid: series instance UID not found for patient %2, study %3. "
+                 "All records belonging to this series will be ignored.")
+              .arg(directoryName)
+              .arg(patientsName.c_str())
+              .arg(studyInstanceUID.c_str())
+            );
             success = false;
             continue;
           }
@@ -592,9 +610,15 @@ bool ctkDICOMIndexer::addDicomdir(const QString& directoryName, bool copyFile/*=
             if (fileRecord->findAndGetOFStringArray(DCM_ReferencedSOPInstanceUIDInFile, sopInstanceUID).bad()
               || fileRecord->findAndGetOFStringArray(DCM_ReferencedFileID,referencedFileName).bad())
             {
-              logger.warn( "DICOMDIR file at "+directoryName+" is invalid: referenced SOP instance UID or file name is invalid for patient "
-                + QString(patientsName.c_str())+", study "+ QString(studyInstanceUID.c_str())+", series "+ QString(seriesInstanceUID.c_str())+
-                ". This file will be ignored.");
+              logger.warn(
+                QString("DICOMDIR file at %1 is invalid: "
+                   "referenced SOP instance UID or file name is invalid for patient %2, study %3, series %4. "
+                   "This file will be ignored.")
+                .arg(directoryName)
+                .arg(patientsName.c_str())
+                .arg(studyInstanceUID.c_str())
+                .arg(seriesInstanceUID.c_str())
+              );
               success = false;
               continue;
             }

Libs/DICOM/Core/ctkDICOMItem.cpp
@@ -848,14 +848,14 @@ QString ctkDICOMItem::TranslateDefinedTermPatientPosition( const QString& dt )
   static QMap<QString, QString> descriptionOfTerms;
   if (!initialized)
   {
-    descriptionOfTerms.insert("HFP",  "Head First - Prone");
-    descriptionOfTerms.insert("HFDR", "Head First - Decubitus Right");
-    descriptionOfTerms.insert("FFDR", "Feet First - Decubitus Right");
-    descriptionOfTerms.insert("FFP",  "Feet First - Prone");
-    descriptionOfTerms.insert("HFS",  "Head First - Supine");
-    descriptionOfTerms.insert("HFDL", "Head First - Decubitus Left");
-    descriptionOfTerms.insert("FFDL", "Feet First - Decubitus Left");
-    descriptionOfTerms.insert("FFS",  "Feet First - Supine");
+    descriptionOfTerms.insert("HFP",  tr("Head First - Prone"));
+    descriptionOfTerms.insert("HFDR", tr("Head First - Decubitus Right"));
+    descriptionOfTerms.insert("FFDR", tr("Feet First - Decubitus Right"));
+    descriptionOfTerms.insert("FFP",  tr("Feet First - Prone"));
+    descriptionOfTerms.insert("HFS",  tr("Head First - Supine"));
+    descriptionOfTerms.insert("HFDL", tr("Head First - Decubitus Left"));
+    descriptionOfTerms.insert("FFDL", tr("Feet First - Decubitus Left"));
+    descriptionOfTerms.insert("FFS",  tr("Feet First - Supine"));
     initialized = true;
   }
 
@@ -876,72 +876,72 @@ QString ctkDICOMItem::TranslateDefinedTermModality( const QString& dt )
   static QMap<QString, QString> descriptionOfTerms;
   if (!initialized)
   {
-    descriptionOfTerms.insert("CR",  "Computed Radiography");
-    descriptionOfTerms.insert("CT",  "Computed Tomography");
-    descriptionOfTerms.insert("MR",  "Magnetic Resonance");
-    descriptionOfTerms.insert("NM",  "Nuclear Medicine");
-    descriptionOfTerms.insert("US",  "Ultrasound");
-    descriptionOfTerms.insert("OT",  "Other");
-    descriptionOfTerms.insert("BI",  "Biomagnetic imaging");
-    descriptionOfTerms.insert("CD",  "Color flow Doppler");
-    descriptionOfTerms.insert("DD",  "Duplex Doppler");
-    descriptionOfTerms.insert("ES",  "Endoscopy");
-    descriptionOfTerms.insert("LS",  "Laser surface scan");
-    descriptionOfTerms.insert("PT",  "Positron emission tomography (PET)");
-    descriptionOfTerms.insert("RG",  "Radiographic imaging (conventional film/screen)");
-    descriptionOfTerms.insert("ST",  "Single-photon emission computed tomograpy (SPECT)");
-    descriptionOfTerms.insert("TG",  "Thermography");
-    descriptionOfTerms.insert("XA",  "X-Ray Aniography");
-    descriptionOfTerms.insert("RF",  "Radio Fluoroscopy");
-    descriptionOfTerms.insert("RTIMAGE",  "Radiotherapy Image");
-    descriptionOfTerms.insert("RTDOSE",  "Radiotherapy Dose");
-    descriptionOfTerms.insert("RTSTRUCT",  "Radiotherapy Structure Set");
-    descriptionOfTerms.insert("RTPLAN",  "Radiotherapy Plan");
-    descriptionOfTerms.insert("RTRECORD",  "RT Treatment Record");
-    descriptionOfTerms.insert("HC",  "Hard Copy");
-    descriptionOfTerms.insert("DX",  "Digital Radiography");
-    descriptionOfTerms.insert("MG",  "Mammography");
-    descriptionOfTerms.insert("IO",  "Intra-oral Radiography");
-    descriptionOfTerms.insert("PX",  "Panoramic X-Ray");
-    descriptionOfTerms.insert("GM",  "General Microscopy");
-    descriptionOfTerms.insert("SM",  "Slide Microscopy");
-    descriptionOfTerms.insert("XC",  "External-camera Photography");
-    descriptionOfTerms.insert("PR",  "Presentation state");
-    descriptionOfTerms.insert("AU",  "Audio");
-    descriptionOfTerms.insert("ECG",  "Electrocardiography");
-    descriptionOfTerms.insert("EPS",  "Cardiac Electrophysiology");
-    descriptionOfTerms.insert("HD",  "Hemodynamic Waveform");
-    descriptionOfTerms.insert("SR",  "SR Document");
-    descriptionOfTerms.insert("IVUS",  "Intravascular Ultrasound");
-    descriptionOfTerms.insert("OP",  "Ophthalmic Photography");
-    descriptionOfTerms.insert("SMR",  "Stereometric Relationship");
-    descriptionOfTerms.insert("OCT",  "Optical Coherence Tomography (non-Ophthalmic)");
-    descriptionOfTerms.insert("OPR",  "Ophthalmic Refraction");
-    descriptionOfTerms.insert("OPV",  "Ophthalmic Visual Field");
-    descriptionOfTerms.insert("OPM",  "Ophthalmic Mapping");
-    descriptionOfTerms.insert("KO",  "Key Object Selection");
-    descriptionOfTerms.insert("SEG",  "Segmentation");
-    descriptionOfTerms.insert("REG",  "Registration");
-    descriptionOfTerms.insert("OPT",  "Ophthalmic Tomography");
-    descriptionOfTerms.insert("BDUS",  "Bone Densitometry (ultrasound)");
-    descriptionOfTerms.insert("BMD",  "Bone Densitometry (X-Ray)");
-    descriptionOfTerms.insert("DOC",  "Document");
+    descriptionOfTerms.insert("CR",  tr("Computed Radiography"));
+    descriptionOfTerms.insert("CT",  tr("Computed Tomography"));
+    descriptionOfTerms.insert("MR",  tr("Magnetic Resonance"));
+    descriptionOfTerms.insert("NM",  tr("Nuclear Medicine"));
+    descriptionOfTerms.insert("US",  tr("Ultrasound"));
+    descriptionOfTerms.insert("OT",  tr("Other"));
+    descriptionOfTerms.insert("BI",  tr("Biomagnetic imaging"));
+    descriptionOfTerms.insert("CD",  tr("Color flow Doppler"));
+    descriptionOfTerms.insert("DD",  tr("Duplex Doppler"));
+    descriptionOfTerms.insert("ES",  tr("Endoscopy"));
+    descriptionOfTerms.insert("LS",  tr("Laser surface scan"));
+    descriptionOfTerms.insert("PT",  tr("Positron emission tomography (PET)"));
+    descriptionOfTerms.insert("RG",  tr("Radiographic imaging (conventional film/screen)"));
+    descriptionOfTerms.insert("ST",  tr("Single-photon emission computed tomograpy (SPECT)"));
+    descriptionOfTerms.insert("TG",  tr("Thermography"));
+    descriptionOfTerms.insert("XA",  tr("X-Ray Aniography"));
+    descriptionOfTerms.insert("RF",  tr("Radio Fluoroscopy"));
+    descriptionOfTerms.insert("RTIMAGE",  tr("Radiotherapy Image"));
+    descriptionOfTerms.insert("RTDOSE",  tr("Radiotherapy Dose"));
+    descriptionOfTerms.insert("RTSTRUCT",  tr("Radiotherapy Structure Set"));
+    descriptionOfTerms.insert("RTPLAN",  tr("Radiotherapy Plan"));
+    descriptionOfTerms.insert("RTRECORD",  tr("RT Treatment Record"));
+    descriptionOfTerms.insert("HC",  tr("Hard Copy"));
+    descriptionOfTerms.insert("DX",  tr("Digital Radiography"));
+    descriptionOfTerms.insert("MG",  tr("Mammography"));
+    descriptionOfTerms.insert("IO",  tr("Intra-oral Radiography"));
+    descriptionOfTerms.insert("PX",  tr("Panoramic X-Ray"));
+    descriptionOfTerms.insert("GM",  tr("General Microscopy"));
+    descriptionOfTerms.insert("SM",  tr("Slide Microscopy"));
+    descriptionOfTerms.insert("XC",  tr("External-camera Photography"));
+    descriptionOfTerms.insert("PR",  tr("Presentation state"));
+    descriptionOfTerms.insert("AU",  tr("Audio"));
+    descriptionOfTerms.insert("ECG",  tr("Electrocardiography"));
+    descriptionOfTerms.insert("EPS",  tr("Cardiac Electrophysiology"));
+    descriptionOfTerms.insert("HD",  tr("Hemodynamic Waveform"));
+    descriptionOfTerms.insert("SR",  tr("SR Document"));
+    descriptionOfTerms.insert("IVUS",  tr("Intravascular Ultrasound"));
+    descriptionOfTerms.insert("OP",  tr("Ophthalmic Photography"));
+    descriptionOfTerms.insert("SMR",  tr("Stereometric Relationship"));
+    descriptionOfTerms.insert("OCT",  tr("Optical Coherence Tomography (non-Ophthalmic)"));
+    descriptionOfTerms.insert("OPR",  tr("Ophthalmic Refraction"));
+    descriptionOfTerms.insert("OPV",  tr("Ophthalmic Visual Field"));
+    descriptionOfTerms.insert("OPM",  tr("Ophthalmic Mapping"));
+    descriptionOfTerms.insert("KO",  tr("Key Object Selection"));
+    descriptionOfTerms.insert("SEG",  tr("Segmentation"));
+    descriptionOfTerms.insert("REG",  tr("Registration"));
+    descriptionOfTerms.insert("OPT",  tr("Ophthalmic Tomography"));
+    descriptionOfTerms.insert("BDUS",  tr("Bone Densitometry (ultrasound)"));
+    descriptionOfTerms.insert("BMD",  tr("Bone Densitometry (X-Ray)"));
+    descriptionOfTerms.insert("DOC",  tr("Document"));
 
     // retired terms (but probably still in use)
-    descriptionOfTerms.insert("DS",  "Digital Subtraction Angiography");
-    descriptionOfTerms.insert("CF",  "Cinefluorography");
-    descriptionOfTerms.insert("DF",  "Digital fluoroscopy");
-    descriptionOfTerms.insert("VF",  "Videofluorography");
-    descriptionOfTerms.insert("AS",  "Angioscopy");
-    descriptionOfTerms.insert("CS",  "Cystoscopy");
-    descriptionOfTerms.insert("EC",  "Echocardiography");
-    descriptionOfTerms.insert("LP",  "Laparoscopy");
-    descriptionOfTerms.insert("FA",  "Fluorescein angiography ");
-    descriptionOfTerms.insert("CP",  "Culposcopy");
-    descriptionOfTerms.insert("DM",  "Digital microscopy");
-    descriptionOfTerms.insert("FS",  "Fundoscopy");
-    descriptionOfTerms.insert("MA",  "Magnetic resonance angiography");
-    descriptionOfTerms.insert("MS",  "Magnetic resonance spectroscopy");
+    descriptionOfTerms.insert("DS",  tr("Digital Subtraction Angiography"));
+    descriptionOfTerms.insert("CF",  tr("Cinefluorography"));
+    descriptionOfTerms.insert("DF",  tr("Digital fluoroscopy"));
+    descriptionOfTerms.insert("VF",  tr("Videofluorography"));
+    descriptionOfTerms.insert("AS",  tr("Angioscopy"));
+    descriptionOfTerms.insert("CS",  tr("Cystoscopy"));
+    descriptionOfTerms.insert("EC",  tr("Echocardiography"));
+    descriptionOfTerms.insert("LP",  tr("Laparoscopy"));
+    descriptionOfTerms.insert("FA",  tr("Fluorescein angiography "));
+    descriptionOfTerms.insert("CP",  tr("Culposcopy"));
+    descriptionOfTerms.insert("DM",  tr("Digital microscopy"));
+    descriptionOfTerms.insert("FS",  tr("Fundoscopy"));
+    descriptionOfTerms.insert("MA",  tr("Magnetic resonance angiography"));
+    descriptionOfTerms.insert("MS",  tr("Magnetic resonance spectroscopy"));
     initialized = true;
   }
 
@@ -969,10 +969,12 @@ QString ctkDICOMItem::TagKeyStripped( const DcmTag& tag )
 QString ctkDICOMItem::TagDescription( const DcmTag& tag )
 {
   if (!dcmDataDict.isDictionaryLoaded())
-    return QString("<no DICOM dictionary loaded. application broken>");
+  {
+    return tr("Unknown (no DICOM dictionary loaded)");
+  }
   const DcmDataDictionary& globalDict = dcmDataDict.rdlock();
   const DcmDictEntry* entry = globalDict.findEntry(tag, NULL);
-  QString returnName("Unknown");
+  QString returnName = tr("Unknown");
   if (entry)
   {
     returnName = entry->getTagName();
@@ -987,7 +989,10 @@ QString ctkDICOMItem::TagDescription( const DcmTag& tag )
 
 QString ctkDICOMItem::TagVR( const DcmTag& tag )
 {
-  if (!dcmDataDict.isDictionaryLoaded()) return QString("<no DICOM dictionary loaded. application broken>");
+  if (!dcmDataDict.isDictionaryLoaded())
+  {
+    return tr("Unknown (no DICOM dictionary loaded)");
+  }
   const DcmDataDictionary& globalDataDict = dcmDataDict.rdlock();
   const DcmDictEntry* entry = globalDataDict.findEntry(tag, NULL);
   QString returnVR("UN");

Libs/DICOM/Core/ctkDICOMModel.cpp
@@ -123,23 +123,23 @@ ctkDICOMModelPrivate::~ctkDICOMModelPrivate()
 void ctkDICOMModelPrivate::init()
 {
   QMap<int, QVariant> data;
-  data[Qt::DisplayRole] = QString("Name");
+  data[Qt::DisplayRole] = ctkDICOMModel::tr("Name");
   this->Headers << data;
-  data[Qt::DisplayRole] = QString("Age");
+  data[Qt::DisplayRole] = ctkDICOMModel::tr("Age");
   this->Headers << data;
-  data[Qt::DisplayRole] = QString("Scan");
+  data[Qt::DisplayRole] = ctkDICOMModel::tr("Scan");
   this->Headers << data;
-  data[Qt::DisplayRole] = QString("Date");
+  data[Qt::DisplayRole] = ctkDICOMModel::tr("Date");
   this->Headers << data;
-  data[Qt::DisplayRole] = QString("Subject ID");
+  data[Qt::DisplayRole] = ctkDICOMModel::tr("Subject ID");
   this->Headers << data;
-  data[Qt::DisplayRole] = QString("Number");
+  data[Qt::DisplayRole] = ctkDICOMModel::tr("Number");
   this->Headers << data;
-  data[Qt::DisplayRole] = QString("Institution");
+  data[Qt::DisplayRole] = ctkDICOMModel::tr("Institution");
   this->Headers << data;
-  data[Qt::DisplayRole] = QString("Referrer");
+  data[Qt::DisplayRole] = ctkDICOMModel::tr("Referrer");
   this->Headers << data;
-  data[Qt::DisplayRole] = QString("Performer");
+  data[Qt::DisplayRole] = ctkDICOMModel::tr("Performer");
   this->Headers << data;
 }
 
@@ -507,13 +507,13 @@ QVariant ctkDICOMModel::data ( const QModelIndex & dataIndex, int role ) const
   QVariant dataValue=d->value(parentIndex, dataIndex.row(), field);
   if (dataValue.isNull())
   {
-    if (columnName.compare("Name")==0)
+    if (columnName.compare(tr("Name"))==0)
     {
-      return QString("No description");
+      return tr("No description");
     }
   }
 
-  if (columnName.compare("Name")==0)
+  if (columnName.compare(tr("Name"))==0)
     {
     OFString dicomName = dataValue.toString().toStdString().c_str();
     OFString formattedName;

Libs/DICOM/Core/ctkDICOMQuery.cpp
@@ -73,7 +73,7 @@ public:
       if (this->query)
         {
         logger.debug ( "FIND RESPONSE" );
-        emit this->query->debug("Got a find response!");
+        emit this->query->debug(/*no tr*/"Got a find response!");
         return this->DcmSCU::handleFINDResponse(presID, response, waitForNextResponse);
         }
       return DIMSE_NULLKEY;
@@ -260,12 +260,12 @@ bool ctkDICOMQuery::query(ctkDICOMDatabase& database )
   if ( database.database().isOpen() )
     {
     logger.debug ( "DB open in Query" );
-    emit progress("DB open in Query");
+    emit progress(tr("DB open in Query"));
     }
   else
     {
     logger.debug ( "DB not open in Query" );
-    emit progress("DB not open in Query");
+    emit progress(tr("DB not open in Query"));
     }
   emit progress(0);
   if (d->Canceled) {return false;}
@@ -278,7 +278,7 @@ bool ctkDICOMQuery::query(ctkDICOMDatabase& database )
   d->SCU.setPeerPort ( this->port() );
 
   logger.error ( "Setting Transfer Syntaxes" );
-  emit progress("Setting Transfer Syntaxes");
+  emit progress(tr("Setting Transfer Syntaxes"));
   emit progress(10);
   if (d->Canceled) {return false;}
 
@@ -292,12 +292,12 @@ bool ctkDICOMQuery::query(ctkDICOMDatabase& database )
   if ( !d->SCU.initNetwork().good() )
     {
     logger.error( "Error initializing the network" );
-    emit progress("Error initializing the network");
+    emit progress(tr("Error initializing the network"));
     emit progress(100);
     return false;
     }
   logger.debug ( "Negotiating Association" );
-  emit progress("Negotiating Association");
+  emit progress(tr("Negotiating Association"));
   emit progress(20);
   if (d->Canceled) {return false;}
 
@@ -305,7 +305,7 @@ bool ctkDICOMQuery::query(ctkDICOMDatabase& database )
   if (result.bad())
     {
     logger.error( "Error negotiating the association: " + QString(result.text()) );
-    emit progress("Error negotiating the association");
+    emit progress(tr("Error negotiating the association"));
     emit progress(100);
     return false;
     }
@@ -406,12 +406,12 @@ bool ctkDICOMQuery::query(ctkDICOMDatabase& database )
   if ( presentationContext == 0 )
     {
     logger.error ( "Failed to find acceptable presentation context" );
-    emit progress("Failed to find acceptable presentation context");
+    emit progress(tr("Failed to find acceptable presentation context"));
     }
   else
     {
     logger.info ( "Found useful presentation context" );
-    emit progress("Found useful presentation context");
+    emit progress(tr("Found useful presentation context"));
     }
   emit progress(40);
   if (d->Canceled) {return false;}
@@ -420,13 +420,13 @@ bool ctkDICOMQuery::query(ctkDICOMDatabase& database )
   if ( !status.good() )
     {
     logger.error ( "Find failed" );
-    emit progress("Find failed");
+    emit progress(tr("Find failed"));
     d->SCU.closeAssociation ( DCMSCU_RELEASE_ASSOCIATION );
     emit progress(100);
     return false;
     }
   logger.debug ( "Find succeeded");
-  emit progress("Find succeeded");
+  emit progress(tr("Find succeeded"));
   emit progress(50);
   if (d->Canceled) {return false;}
 
@@ -439,7 +439,7 @@ bool ctkDICOMQuery::query(ctkDICOMDatabase& database )
       OFString StudyInstanceUID;
       dataset->findAndGetOFString ( DCM_StudyInstanceUID, StudyInstanceUID );
       d->addStudyInstanceUIDAndDataset ( StudyInstanceUID.c_str(), dataset );
-      emit progress(QString("Processing: ") + QString(StudyInstanceUID.c_str()));
+      emit progress(tr("Processing Study: ") + QString(StudyInstanceUID.c_str()));
       emit progress(50);
       if (d->Canceled) {return false;}
       }
@@ -472,7 +472,7 @@ bool ctkDICOMQuery::query(ctkDICOMDatabase& database )
     studyDataset->findAndGetElement(DCM_PatientID, patientID);
 
     logger.debug ( "Starting Series C-FIND for Study: " + StudyInstanceUID );
-    emit progress(QString("Starting Series C-FIND for Study: ") + StudyInstanceUID);
+    emit progress(tr("Starting Series C-FIND for Study: ") + StudyInstanceUID);
     emit progress(50 + (progressRatio * i++));
     if (d->Canceled) {return false;}
 
@@ -497,14 +497,14 @@ bool ctkDICOMQuery::query(ctkDICOMDatabase& database )
           }
         }
       logger.debug ( "Find succeeded on Series level for Study: " + StudyInstanceUID );
-      emit progress(QString("Find succeeded on Series level for Study: ") + StudyInstanceUID);
+      emit progress(tr("Find succeeded on Series level for Study: ") + StudyInstanceUID);
       emit progress(50 + (progressRatio * i++));
       if (d->Canceled) {return false;}
       }
     else
       {
       logger.error ( "Find on Series level failed for Study: " + StudyInstanceUID );
-      emit progress(QString("Find on Series level failed for Study: ") + StudyInstanceUID);
+      emit progress(tr("Find on Series level failed for Study: ") + StudyInstanceUID);
       }
     emit progress(50 + (progressRatio * i++));
     if (d->Canceled) {return false;}

commit e1e560f4e2a21a43f4b2027daa8c28e9cab55f90

COMP: Fix DICOM/Core build ensuring Qt headers can always be included
This commit fixes a regression introduced in c819ae737 (BUG: Mark translatable
strings in DICOM/Core)

Libs/DICOM/Core/ctkDICOMDisplayedFieldGeneratorRadiotherapySeriesDescriptionRule.h
@@ -22,7 +22,7 @@
 #define __ctkDICOMDisplayedFieldGeneratorRadiotherapySeriesDescriptionRule_h
 
 // Qt includes
-#include <QCoreApplication.h>
+#include <QCoreApplication>
 #include <QStringList>
 
 #include "ctkDICOMDisplayedFieldGeneratorAbstractRule.h"

Libs/DICOM/Core/ctkDICOMItem.h
@@ -27,7 +27,7 @@
 
 #include <dcmtk/dcmdata/dcdatset.h> // DCMTK DcmDataset
 
-#include <QCoreApplication.h>
+#include <QCoreApplication>
 #include <QtCore>
 
 class DcmDataDictionary;