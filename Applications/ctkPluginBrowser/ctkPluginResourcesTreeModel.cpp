3437bee743f4c4a2dbcf8abefd7f46d9812639ab
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2023-06-13 10:14:26 -0400

    COMP: Fix QFlags deprecation warnings
    This commit fixes warnings like the following:
    
    /path/to/CTK/Libs/Widgets/ctkRangeSlider.cpp:776:26: warning: ‘constexpr QFlags<T>::QFlags(QFlags<T>::Zero) [with Enum = ctkRangeSliderPrivate::Handle; QFlags<T>::Zero = int QFlags<ctkRangeSliderPrivate::Handle>::Private::*]’ is deprecated: Use default constructor instead [-Wdeprecated-declarations]
      776 |   d->m_SelectedHandles = 0;
          |                          ^

--- Applications/ctkPluginBrowser/ctkPluginResourcesTreeModel.cpp
+++ Applications/ctkPluginBrowser/ctkPluginResourcesTreeModel.cpp
@@ -204,7 +204,9 @@ QVariant ctkPluginResourcesTreeModel::data(const QModelIndex &index, int role) c
 Qt::ItemFlags ctkPluginResourcesTreeModel::flags(const QModelIndex &index) const
 {
   if (!index.isValid())
-    return 0;
+  {
+    return Qt::ItemFlags();
+  }
 
   return Qt::ItemIsEnabled | Qt::ItemIsSelectable;
 }
