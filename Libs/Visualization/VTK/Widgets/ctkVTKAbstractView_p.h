01963f446f7917f1068fc231d0014c20bced42d7
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2023-06-14 00:43:44 -0400

    COMP: Fix deprecated use of QTime::start()/elapsed() in ctkVTKAbstractView
    This commit fixes the following warnings:
    
    /path/to/CTK/Libs/Visualization/VTK/Widgets/ctkVTKAbstractView.cpp:207:26: warning: ‘void QTime::start()’ is deprecated: Use QElapsedTimer instead [-Wdeprecated-declarations]
      207 |     d->RequestTime.start();
          |                          ^
    
    /path/to/CTK/Libs/Visualization/VTK/Widgets/ctkVTKAbstractView.cpp:210:35: warning: ‘int QTime::elapsed() const’ is deprecated: Use QElapsedTimer instead [-Wdeprecated-declarations]
      210 |   else if (d->RequestTime.elapsed() > msecsBeforeRender)
          |                                   ^

--- Libs/Visualization/VTK/Widgets/ctkVTKAbstractView_p.h
+++ Libs/Visualization/VTK/Widgets/ctkVTKAbstractView_p.h
@@ -22,6 +22,10 @@
 #define __ctkVTKAbstractView_p_h
 
 // Qt includes
+#include <QtGlobal>
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 14, 0))
+#include <QElapsedTimer>
+#endif
 #include <QObject>
 #include <QTime>
 class QTimer;
@@ -64,7 +68,11 @@ public:
   vtkSmartPointer<vtkRenderWindow>              RenderWindow;
 #endif
   QTimer*                                       RequestTimer;
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 14, 0))
+  QElapsedTimer                                 RequestTime;
+#else
   QTime                                         RequestTime;
+#endif
   bool                                          RenderEnabled;
   double                                        MaximumUpdateRate;
   bool                                          FPSVisible;
