commontk/CTK

commit 7b48c481ab831215f7b3a98852e5540ff97c5531

ENH: Add support for PBR material properties in material property widgets
ctkMaterialPropertyWidget can now edit material properties for both non-PBR (Phong, Gouraud) and PBR interpolation.
PBR material properties are `diffuse` (shared with non-PBR modes) and `metallic` and `roughness` (matching names used in VTK and glTF).
An interpolation mode selector is added (hidden by default for backward compatibility) to allow easy switching between interpolation modes.

ctkVTKSurfaceMaterialPropertyWidget is updated to get/set associated properties in VTK.

ctkMaterialPropertyPreviewLabel is updated so that it can display both PBR and non-PBR material settings. Preview of PBR settings is a rough approximation.

Libs/Visualization/VTK/Widgets/ctkVTKSurfaceMaterialPropertyWidget.cpp
@@ -38,6 +38,10 @@ public:
   ctkVTKSurfaceMaterialPropertyWidgetPrivate(ctkVTKSurfaceMaterialPropertyWidget& object);
   vtkSmartPointer<vtkProperty> Property;
   double                       SettingColor;
+
+  // Flag that indicates that the GUI is being updated from the VTK property,
+  // therefore GUI changes should not trigger VTK property update.
+  bool IsUpdatingGUI;
 };
 
 //-----------------------------------------------------------------------------
@@ -45,6 +49,7 @@ ctkVTKSurfaceMaterialPropertyWidgetPrivate::ctkVTKSurfaceMaterialPropertyWidgetP
   :q_ptr(&object)
 {
   this->SettingColor = false;
+  this->IsUpdatingGUI = false;
 }
 
 //-----------------------------------------------------------------------------
@@ -98,13 +103,32 @@ void ctkVTKSurfaceMaterialPropertyWidget::updateFromProperty()
     {
     return;
     }
+  if (d->IsUpdatingGUI)
+    {
+    // Update is already in progress
+    return;
+    }
+  d->IsUpdatingGUI = true;
   double* c = d->Property->GetColor();
   this->setColor(QColor::fromRgbF(qMin(c[0],1.), qMin(c[1], 1.), qMin(c[2],1.)));
   this->setOpacity(d->Property->GetOpacity());
+
+  switch (d->Property->GetInterpolation())
+    {
+    case VTK_FLAT: this->setInterpolationMode(InterpolationFlat); break;
+    case VTK_GOURAUD: this->setInterpolationMode(InterpolationGouraud); break;
+    case VTK_PHONG: this->setInterpolationMode(InterpolationPhong); break;
+    case VTK_PBR: this->setInterpolationMode(InterpolationPBR); break;
+    }
+
   this->setAmbient(d->Property->GetAmbient());
   this->setDiffuse(d->Property->GetDiffuse());
   this->setSpecular(d->Property->GetSpecular());
   this->setSpecularPower(d->Property->GetSpecularPower());
+
+  this->setMetallic(d->Property->GetMetallic());
+  this->setRoughness(d->Property->GetRoughness());
+  d->IsUpdatingGUI = false;
 }
 
 // --------------------------------------------------------------------------
@@ -141,6 +165,26 @@ void ctkVTKSurfaceMaterialPropertyWidget::onOpacityChanged(double newOpacity)
     // the value might have changed since we fired the signal, use the current
     // up-to-date value then.
     d->Property->SetOpacity(this->opacity());
+    }
+}
+
+// --------------------------------------------------------------------------
+void ctkVTKSurfaceMaterialPropertyWidget::onInterpolationModeChanged(
+  ctkMaterialPropertyWidget::InterpolationMode newInterpolationMode)
+{
+  Q_D(ctkVTKSurfaceMaterialPropertyWidget);
+  this->Superclass::onInterpolationModeChanged(newInterpolationMode);
+  if (d->Property.GetPointer() != 0)
+    {
+    // the value might have changed since we fired the signal, use the current
+    // up-to-date value then.
+    switch (this->interpolationMode())
+      {
+      case InterpolationFlat: d->Property->SetInterpolationToFlat(); break;
+      case InterpolationGouraud: d->Property->SetInterpolationToGouraud(); break;
+      case InterpolationPhong: d->Property->SetInterpolationToPhong(); break;
+      case InterpolationPBR: d->Property->SetInterpolationToPBR(); break;
+      }
     }
 }
 
@@ -193,6 +237,32 @@ void ctkVTKSurfaceMaterialPropertyWidget::onSpecularPowerChanged(double newSpecu
     // the value might have changed since we fired the signal, use the current
     // up-to-date value then.
     d->Property->SetSpecularPower(this->specularPower());
+    }
+}
+
+// --------------------------------------------------------------------------
+void ctkVTKSurfaceMaterialPropertyWidget::onMetallicChanged(double newMetallic)
+{
+  Q_D(ctkVTKSurfaceMaterialPropertyWidget);
+  this->Superclass::onMetallicChanged(newMetallic);
+  if (d->Property.GetPointer() != 0)
+    {
+    // the value might have changed since we fired the signal, use the current
+    // up-to-date value then.
+    d->Property->SetMetallic(this->metallic());
+    }
+}
+
+// --------------------------------------------------------------------------
+void ctkVTKSurfaceMaterialPropertyWidget::onRoughnessChanged(double newRoughness)
+{
+  Q_D(ctkVTKSurfaceMaterialPropertyWidget);
+  this->Superclass::onRoughnessChanged(newRoughness);
+  if (d->Property.GetPointer() != 0)
+    {
+    // the value might have changed since we fired the signal, use the current
+    // up-to-date value then.
+    d->Property->SetRoughness(this->roughness());
     }
 }
 

Libs/Visualization/VTK/Widgets/ctkVTKSurfaceMaterialPropertyWidget.h
@@ -56,10 +56,13 @@ protected:
 
   virtual void onColorChanged(const QColor& newColor);
   virtual void onOpacityChanged(double newOpacity);
+  virtual void onInterpolationModeChanged(ctkMaterialPropertyWidget::InterpolationMode newMode);
   virtual void onAmbientChanged(double newAmbient);
   virtual void onDiffuseChanged(double newDiffuse);
   virtual void onSpecularChanged(double newSpecular);
   virtual void onSpecularPowerChanged(double newSpecularPower);
+  virtual void onMetallicChanged(double newMetallic);
+  virtual void onRoughnessChanged(double newRoughness);
   virtual void onBackfaceCullingChanged(bool newBackfaceCulling);
 
 private:

Libs/Widgets/Resources/UI/ctkMaterialPropertyWidget.ui
@@ -6,8 +6,8 @@
    <rect>
     <x>0</x>
     <y>0</y>
-    <width>248</width>
-    <height>212</height>
+    <width>301</width>
+    <height>284</height>
    </rect>
   </property>
   <property name="windowTitle">
@@ -17,7 +17,16 @@
    <property name="fieldGrowthPolicy">
     <enum>QFormLayout::ExpandingFieldsGrow</enum>
    </property>
-   <property name="margin">
+   <property name="leftMargin">
+    <number>0</number>
+   </property>
+   <property name="topMargin">
+    <number>0</number>
+   </property>
+   <property name="rightMargin">
+    <number>0</number>
+   </property>
+   <property name="bottomMargin">
     <number>0</number>
    </property>
    <item row="0" column="0">
@@ -70,14 +79,14 @@
      </property>
     </widget>
    </item>
-   <item row="2" column="0">
+   <item row="3" column="0">
     <widget class="QLabel" name="AmbientLabel">
      <property name="text">
       <string>Ambient:</string>
      </property>
     </widget>
    </item>
-   <item row="2" column="1">
+   <item row="3" column="1">
     <widget class="ctkSliderWidget" name="AmbientSliderSpinBox">
      <property name="singleStep">
       <double>0.010000000000000</double>
@@ -87,14 +96,14 @@
      </property>
     </widget>
    </item>
-   <item row="3" column="0">
+   <item row="4" column="0">
     <widget class="QLabel" name="DiffuseLabel">
      <property name="text">
       <string>Diffuse:</string>
      </property>
     </widget>
    </item>
-   <item row="3" column="1">
+   <item row="4" column="1">
     <widget class="ctkSliderWidget" name="DiffuseSliderSpinBox">
      <property name="singleStep">
       <double>0.010000000000000</double>
@@ -107,14 +116,14 @@
      </property>
     </widget>
    </item>
-   <item row="4" column="0">
+   <item row="5" column="0">
     <widget class="QLabel" name="SpecularLabel">
      <property name="text">
       <string>Specular:</string>
      </property>
     </widget>
    </item>
-   <item row="4" column="1">
+   <item row="5" column="1">
     <widget class="ctkSliderWidget" name="SpecularSliderSpinBox">
      <property name="singleStep">
       <double>0.010000000000000</double>
@@ -124,7 +133,7 @@
      </property>
     </widget>
    </item>
-   <item row="5" column="0">
+   <item row="6" column="0">
     <widget class="QLabel" name="SpecularPowerLabel">
      <property name="toolTip">
       <string>Specular power</string>
@@ -134,7 +143,7 @@
      </property>
     </widget>
    </item>
-   <item row="5" column="1">
+   <item row="6" column="1">
     <widget class="ctkSliderWidget" name="SpecularPowerSliderSpinBox">
      <property name="decimals">
       <number>1</number>
@@ -150,28 +159,28 @@
      </property>
     </widget>
    </item>
-   <item row="6" column="0">
+   <item row="9" column="0">
     <widget class="QLabel" name="BackfaceCullingLabel">
      <property name="text">
       <string>Backface Culling:</string>
      </property>
     </widget>
    </item>
-   <item row="6" column="1">
+   <item row="9" column="1">
     <widget class="QCheckBox" name="BackfaceCullingCheckBox">
      <property name="text">
       <string/>
      </property>
     </widget>
    </item>
-   <item row="7" column="0">
+   <item row="10" column="0">
     <widget class="QLabel" name="PreviewLabel">
      <property name="text">
       <string>Preview:</string>
      </property>
     </widget>
    </item>
-   <item row="7" column="1">
+   <item row="10" column="1">
     <layout class="QHBoxLayout" name="horizontalLayout_2">
      <item>
       <widget class="ctkMaterialPropertyPreviewLabel" name="MaterialPropertyPreviewLabel">
@@ -245,6 +254,77 @@
      </item>
     </layout>
    </item>
+   <item row="7" column="0">
+    <widget class="QLabel" name="MetallicLabel">
+     <property name="text">
+      <string>Metallic:</string>
+     </property>
+    </widget>
+   </item>
+   <item row="8" column="0">
+    <widget class="QLabel" name="RoughnessLabel">
+     <property name="text">
+      <string>Roughness:</string>
+     </property>
+    </widget>
+   </item>
+   <item row="7" column="1">
+    <widget class="ctkSliderWidget" name="MetallicSliderSpinBox">
+     <property name="singleStep">
+      <double>0.010000000000000</double>
+     </property>
+     <property name="maximum">
+      <double>1.000000000000000</double>
+     </property>
+    </widget>
+   </item>
+   <item row="8" column="1">
+    <widget class="ctkSliderWidget" name="RoughnessSliderSpinBox">
+     <property name="singleStep">
+      <double>0.010000000000000</double>
+     </property>
+     <property name="maximum">
+      <double>1.000000000000000</double>
+     </property>
+    </widget>
+   </item>
+   <item row="2" column="0">
+    <widget class="QLabel" name="InterpolationModeLabel">
+     <property name="text">
+      <string>Mode:</string>
+     </property>
+    </widget>
+   </item>
+   <item row="2" column="1">
+    <widget class="QComboBox" name="InterpolationModeComboBox">
+     <property name="sizePolicy">
+      <sizepolicy hsizetype="Expanding" vsizetype="Fixed">
+       <horstretch>0</horstretch>
+       <verstretch>0</verstretch>
+      </sizepolicy>
+     </property>
+     <item>
+      <property name="text">
+       <string>Flat</string>
+      </property>
+     </item>
+     <item>
+      <property name="text">
+       <string>Gouraud</string>
+      </property>
+     </item>
+     <item>
+      <property name="text">
+       <string>Phong</string>
+      </property>
+     </item>
+     <item>
+      <property name="text">
+       <string>PBR</string>
+      </property>
+     </item>
+    </widget>
+   </item>
   </layout>
  </widget>
  <customwidgets>
@@ -273,12 +353,12 @@
    <slot>setAmbient(double)</slot>
    <hints>
     <hint type="sourcelabel">
-     <x>168</x>
-     <y>67</y>
+     <x>257</x>
+     <y>96</y>
     </hint>
     <hint type="destinationlabel">
-     <x>116</x>
-     <y>209</y>
+     <x>119</x>
+     <y>280</y>
     </hint>
    </hints>
   </connection>
@@ -289,12 +369,12 @@
    <slot>setDiffuse(double)</slot>
    <hints>
     <hint type="sourcelabel">
-     <x>202</x>
-     <y>100</y>
+     <x>291</x>
+     <y>121</y>
     </hint>
     <hint type="destinationlabel">
-     <x>116</x>
-     <y>209</y>
+     <x>119</x>
+     <y>280</y>
     </hint>
    </hints>
   </connection>
@@ -305,12 +385,12 @@
    <slot>setSpecular(double)</slot>
    <hints>
     <hint type="sourcelabel">
-     <x>222</x>
-     <y>126</y>
+     <x>300</x>
+     <y>146</y>
     </hint>
     <hint type="destinationlabel">
-     <x>116</x>
-     <y>209</y>
+     <x>119</x>
+     <y>280</y>
     </hint>
    </hints>
   </connection>
@@ -321,12 +401,12 @@
    <slot>setSpecularPower(double)</slot>
    <hints>
     <hint type="sourcelabel">
-     <x>234</x>
-     <y>152</y>
+     <x>300</x>
+     <y>171</y>
     </hint>
     <hint type="destinationlabel">
-     <x>116</x>
-     <y>209</y>
+     <x>119</x>
+     <y>280</y>
     </hint>
    </hints>
   </connection>
@@ -341,8 +421,8 @@
      <y>14</y>
     </hint>
     <hint type="destinationlabel">
-     <x>103</x>
-     <y>191</y>
+     <x>119</x>
+     <y>280</y>
     </hint>
    </hints>
   </connection>
@@ -353,12 +433,44 @@
    <slot>setOpacity(double)</slot>
    <hints>
     <hint type="sourcelabel">
-     <x>105</x>
-     <y>24</y>
+     <x>194</x>
+     <y>46</y>
     </hint>
     <hint type="destinationlabel">
-     <x>103</x>
+     <x>119</x>
+     <y>280</y>
+    </hint>
+   </hints>
+  </connection>
+  <connection>
+   <sender>MetallicSliderSpinBox</sender>
+   <signal>valueChanged(double)</signal>
+   <receiver>MaterialPropertyPreviewLabel</receiver>
+   <slot>setMetallic(double)</slot>
+   <hints>
+    <hint type="sourcelabel">
+     <x>141</x>
      <y>191</y>
+    </hint>
+    <hint type="destinationlabel">
+     <x>109</x>
+     <y>260</y>
+    </hint>
+   </hints>
+  </connection>
+  <connection>
+   <sender>RoughnessSliderSpinBox</sender>
+   <signal>valueChanged(double)</signal>
+   <receiver>MaterialPropertyPreviewLabel</receiver>
+   <slot>setRoughness(double)</slot>
+   <hints>
+    <hint type="sourcelabel">
+     <x>102</x>
+     <y>213</y>
+    </hint>
+    <hint type="destinationlabel">
+     <x>104</x>
+     <y>258</y>
     </hint>
    </hints>
   </connection>

Libs/Widgets/ctkMaterialPropertyPreviewLabel.cpp
@@ -42,12 +42,15 @@ public:
   ctkMaterialPropertyPreviewLabelPrivate(ctkMaterialPropertyPreviewLabel& object);
 
   QColor Color;
+  bool InterpolationPBR;
   double Opacity;
 
   double Ambient;
   double Diffuse;
   double Specular;
   double SpecularPower;
+  double Metallic;
+  double Roughness;
 
   double GridOpacity;
 };
@@ -57,11 +60,14 @@ ctkMaterialPropertyPreviewLabelPrivate::ctkMaterialPropertyPreviewLabelPrivate(c
   :q_ptr(&object)
 {
   this->Color = Qt::white;
+  this->InterpolationPBR = false;
   this->Opacity = 1.;
   this->Ambient = 0.0;
   this->Diffuse = 1.0;
   this->Specular = 0.0;
   this->SpecularPower = 1;
+  this->Metallic = 0.0;
+  this->Roughness = 0.5;
 
   this->GridOpacity = 0.6;
 }
@@ -84,12 +90,30 @@ ctkMaterialPropertyPreviewLabel::ctkMaterialPropertyPreviewLabel(
   , d_ptr(new ctkMaterialPropertyPreviewLabelPrivate(*this))
 {
   Q_D(ctkMaterialPropertyPreviewLabel);
+  d->InterpolationPBR = false;
   d->Color = color;
   d->Opacity = opacity;
   d->Ambient = ambient;
   d->Diffuse = diffuse;
   d->Specular = specular;
   d->SpecularPower = specularPower;
+}
+
+//-----------------------------------------------------------------------------
+ctkMaterialPropertyPreviewLabel::ctkMaterialPropertyPreviewLabel(
+  const QColor& color, double opacity,
+  double diffuse, double metallic, double roughness,
+  QWidget *newParent)
+  : QFrame(newParent)
+  , d_ptr(new ctkMaterialPropertyPreviewLabelPrivate(*this))
+{
+  Q_D(ctkMaterialPropertyPreviewLabel);
+  d->InterpolationPBR = true;
+  d->Color = color;
+  d->Opacity = opacity;
+  d->Diffuse = diffuse;
+  d->Metallic = metallic;
+  d->Roughness = roughness;
 }
 
 //-----------------------------------------------------------------------------
@@ -158,6 +182,36 @@ double ctkMaterialPropertyPreviewLabel::specularPower()const
 }
 
 //-----------------------------------------------------------------------------
+void ctkMaterialPropertyPreviewLabel::setMetallic(double newMetallic)
+{
+  Q_D(ctkMaterialPropertyPreviewLabel);
+  d->Metallic = newMetallic;
+  this->update();
+}
+
+//-----------------------------------------------------------------------------
+double ctkMaterialPropertyPreviewLabel::metallic()const
+{
+  Q_D(const ctkMaterialPropertyPreviewLabel);
+  return d->Metallic;
+}
+
+//-----------------------------------------------------------------------------
+void ctkMaterialPropertyPreviewLabel::setRoughness(double newRoughness)
+{
+  Q_D(ctkMaterialPropertyPreviewLabel);
+  d->Roughness = newRoughness;
+  this->update();
+}
+
+//-----------------------------------------------------------------------------
+double ctkMaterialPropertyPreviewLabel::roughness()const
+{
+  Q_D(const ctkMaterialPropertyPreviewLabel);
+  return d->Roughness;
+}
+
+//-----------------------------------------------------------------------------
 void ctkMaterialPropertyPreviewLabel::setColor(const QColor& newColor)
 {
   Q_D(ctkMaterialPropertyPreviewLabel);
@@ -170,6 +224,21 @@ QColor ctkMaterialPropertyPreviewLabel::color()const
 {
   Q_D(const ctkMaterialPropertyPreviewLabel);
   return d->Color;
+}
+
+//-----------------------------------------------------------------------------
+void ctkMaterialPropertyPreviewLabel::setInterpolationPBR(bool pbr)
+{
+  Q_D(ctkMaterialPropertyPreviewLabel);
+  d->InterpolationPBR = pbr;
+  this->update();
+}
+
+//-----------------------------------------------------------------------------
+bool ctkMaterialPropertyPreviewLabel::interpolationPBR()const
+{
+  Q_D(const ctkMaterialPropertyPreviewLabel);
+  return d->InterpolationPBR;
 }
 
 //-----------------------------------------------------------------------------
@@ -237,6 +306,24 @@ void ctkMaterialPropertyPreviewLabel::draw(QImage& image)
   qreal diffuse = d->Diffuse;
   qreal specular = d->Specular;
   qreal specular_power = d->SpecularPower;
+
+  if (d->InterpolationPBR)
+    {
+    // Try to approximate PBR with Phong rendering.
+    // This is a very rough approximation, but instead of trying to make it
+    // more accurate, it would be better to accept an externally-rendered
+    // bitmap (as the appearance could be very complex, especially when
+    // textures and more sophisticated lighting is used).
+    diffuse = (1.0 - d->Metallic) * (0.75 + 0.25 * d->Roughness);
+    specular = d->Diffuse * (0.75 * (1.0 - d->Roughness));
+    specular_power = 20.0 + 30.0 * (1.0 - d->Roughness);
+    // Use ambient to make sure overall brightness (original diffuse lighting) is preserved
+    ambient = d->Diffuse - specular - diffuse;
+    if (ambient < 0)
+      {
+      ambient = 0;
+      }
+    }
 
   int size = qMin(image.width(), image.height());
   int size8 = qMax(size / 8, 1);

Libs/Widgets/ctkMaterialPropertyPreviewLabel.h
@@ -39,29 +39,41 @@ class CTK_WIDGETS_EXPORT ctkMaterialPropertyPreviewLabel : public QFrame
   Q_PROPERTY(double specular READ specular WRITE setSpecular)
   Q_PROPERTY(double specularPower READ specularPower WRITE setSpecularPower)
 
+  Q_PROPERTY(double metallic READ metallic WRITE setMetallic)
+  Q_PROPERTY(double roughness READ roughness WRITE setRoughness)
+
   Q_PROPERTY(QColor color READ color WRITE setColor)
+  Q_PROPERTY(bool interpolationPBR READ interpolationPBR WRITE setInterpolationPBR)
   Q_PROPERTY(double opacity READ opacity WRITE setOpacity)
   Q_PROPERTY(double gridOpacity READ gridOpacity WRITE setGridOpacity)
-public : 
+public :
 
   ctkMaterialPropertyPreviewLabel(QWidget *parent = 0);
+  /// Non-PBR interpolation
   ctkMaterialPropertyPreviewLabel(const QColor& color, double opacity,
     double ambient, double diffuse, double specular, double specularPower,
     QWidget *parent = 0);
+  /// PBR interpolation
+  ctkMaterialPropertyPreviewLabel(const QColor& color, double opacity,
+    double diffuse, double metallic, double roughness,
+    QWidget* parent = 0);
   virtual ~ctkMaterialPropertyPreviewLabel();
-  
+
   double ambient()const;
   double diffuse()const;
   double specular()const;
   double specularPower()const;
-  
+  double metallic()const;
+  double roughness()const;
+
   QColor color()const;
+  bool interpolationPBR()const;
   double opacity()const;
   double gridOpacity()const;
 
   /// Reimplemented to make it square
   virtual int heightForWidth(int w)const;
-  
+
   virtual QSize sizeHint()const;
 public Q_SLOTS:
   /// Valid range: [0,1]
@@ -72,13 +84,18 @@ public Q_SLOTS:
   void setSpecular(double newSpecular);
   /// Valid range: [1,inf[
   void setSpecularPower(double newSpecularPower);
-  
+  /// Valid range: [0,1]
+  void setMetallic(double newMetallic);
+  /// Valid range: [0,1]
+  void setRoughness(double newRoughness);
+
   void setColor(const QColor& newColor);
+  void setInterpolationPBR(bool pbr);
   /// Valid range: [0, 1]
   void setOpacity(double newOpacity);
   void setGridOpacity(double newGridOpacity);
 
-protected: 
+protected:
   void paintEvent(QPaintEvent *);
   void draw(QImage& image);
 
@@ -89,4 +106,3 @@ private :
 };
 
 #endif
-

commit a2c6e3eea8d5faca04deb721aac7594c7e1d8bc4

COMP: Update ctkVTKSurfaceMaterialPropertyWidget to fix build against VTK8/Qt4
This commit fixes a regression introduced in 7b48c481a (ENH: Add support
for PBR material properties in material property widgets) by reporting
a warning if Metallic or Roughness properties are set.

Libs/Visualization/VTK/Widgets/ctkVTKSurfaceMaterialPropertyWidget.cpp
@@ -27,6 +27,7 @@
 // VTK includes
 #include <vtkSmartPointer.h>
 #include <vtkProperty.h>
+#include <vtkVersion.h>
 
 //-----------------------------------------------------------------------------
 class ctkVTKSurfaceMaterialPropertyWidgetPrivate
@@ -118,7 +119,9 @@ void ctkVTKSurfaceMaterialPropertyWidget::updateFromProperty()
     case VTK_FLAT: this->setInterpolationMode(InterpolationFlat); break;
     case VTK_GOURAUD: this->setInterpolationMode(InterpolationGouraud); break;
     case VTK_PHONG: this->setInterpolationMode(InterpolationPhong); break;
+#if VTK_MAJOR_VERSION >= 9
     case VTK_PBR: this->setInterpolationMode(InterpolationPBR); break;
+#endif
     }
 
   this->setAmbient(d->Property->GetAmbient());
@@ -126,8 +129,10 @@ void ctkVTKSurfaceMaterialPropertyWidget::updateFromProperty()
   this->setSpecular(d->Property->GetSpecular());
   this->setSpecularPower(d->Property->GetSpecularPower());
 
+#if VTK_MAJOR_VERSION >= 9
   this->setMetallic(d->Property->GetMetallic());
   this->setRoughness(d->Property->GetRoughness());
+#endif
   d->IsUpdatingGUI = false;
 }
 
@@ -183,7 +188,11 @@ void ctkVTKSurfaceMaterialPropertyWidget::onInterpolationModeChanged(
       case InterpolationFlat: d->Property->SetInterpolationToFlat(); break;
       case InterpolationGouraud: d->Property->SetInterpolationToGouraud(); break;
       case InterpolationPhong: d->Property->SetInterpolationToPhong(); break;
+#if VTK_MAJOR_VERSION >= 9
       case InterpolationPBR: d->Property->SetInterpolationToPBR(); break;
+#else
+      case InterpolationPBR: break;
+#endif
       }
     }
 }
@@ -243,6 +252,7 @@ void ctkVTKSurfaceMaterialPropertyWidget::onSpecularPowerChanged(double newSpecu
 // --------------------------------------------------------------------------
 void ctkVTKSurfaceMaterialPropertyWidget::onMetallicChanged(double newMetallic)
 {
+#if VTK_MAJOR_VERSION >= 9
   Q_D(ctkVTKSurfaceMaterialPropertyWidget);
   this->Superclass::onMetallicChanged(newMetallic);
   if (d->Property.GetPointer() != 0)
@@ -251,11 +261,16 @@ void ctkVTKSurfaceMaterialPropertyWidget::onMetallicChanged(double newMetallic)
     // up-to-date value then.
     d->Property->SetMetallic(this->metallic());
     }
+#else
+  Q_UNUSED(newMetallic);
+  qWarning() << Q_FUNC_INFO << " failed: metallic property is not supported with VTK < 9";
+#endif
 }
 
 // --------------------------------------------------------------------------
 void ctkVTKSurfaceMaterialPropertyWidget::onRoughnessChanged(double newRoughness)
 {
+#if VTK_MAJOR_VERSION >= 9
   Q_D(ctkVTKSurfaceMaterialPropertyWidget);
   this->Superclass::onRoughnessChanged(newRoughness);
   if (d->Property.GetPointer() != 0)
@@ -264,6 +279,10 @@ void ctkVTKSurfaceMaterialPropertyWidget::onRoughnessChanged(double newRoughness
     // up-to-date value then.
     d->Property->SetRoughness(this->roughness());
     }
+#else
+  Q_UNUSED(newRoughness);
+  qWarning() << Q_FUNC_INFO << " failed: roughness property is not supported with VTK < 9";
+#endif
 }
 
 // --------------------------------------------------------------------------