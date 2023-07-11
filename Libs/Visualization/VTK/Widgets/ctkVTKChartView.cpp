commontk/CTK

commit 58f40388136cc2d208f2d705cbdcf09233686a51

ENH: Add access to render window in ctkVTKChartView
This allows applications to configure options such as anti-aliasing, adding more rendering layers, etc.

Libs/Visualization/VTK/Widgets/ctkVTKChartView.cpp
@@ -37,6 +37,7 @@
 #include <vtkOpenGLContextDevice2D.h>
 #include <vtkPlot.h>
 #include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
 
 //----------------------------------------------------------------------------
 static ctkLogger logger("org.commontk.visualization.vtk.widgets.ctkVTKChartView");
@@ -170,6 +171,8 @@ void ctkVTKChartViewPrivate::chartBounds(double* bounds)const
 
 // ----------------------------------------------------------------------------
 // ctkVTKChartView methods
+
+CTK_GET_CPP(ctkVTKChartView, vtkRenderWindow*, renderWindow, RenderWindow);
 
 // ----------------------------------------------------------------------------
 ctkVTKChartView::ctkVTKChartView(QWidget* parentWidget)

Libs/Visualization/VTK/Widgets/ctkVTKChartView.h
@@ -79,7 +79,9 @@ public:
   void chartBounds(double bounds[8])const;
   void setChartUserBounds(double* bounds);
   void chartUserBounds(double* bounds)const;
-  
+
+  /// Get underlying RenderWindow
+  Q_INVOKABLE vtkRenderWindow* renderWindow()const;
 
 public Q_SLOTS:
 

commit 6db997ef404f8e953a8997b021a3a0dd7344aecd

COMP: Update ctkVTKChartView to fix build against VTK 8
This commit fixes a regression introduced in 58f403881 (ENH: Add access
to render window in ctkVTKChartView)

Libs/Visualization/VTK/Widgets/ctkVTKChartView.cpp
@@ -189,7 +189,16 @@ void ctkVTKChartViewPrivate::chartBounds(double* bounds)const
 // ----------------------------------------------------------------------------
 // ctkVTKChartView methods
 
-CTK_GET_CPP(ctkVTKChartView, vtkRenderWindow*, renderWindow, RenderWindow);
+// ----------------------------------------------------------------------------
+vtkRenderWindow* ctkVTKChartView::renderWindow()const
+{
+#ifdef CTK_USE_QVTKOPENGLWIDGET
+  Q_D(const ctkVTKChartView);
+  return d->RenderWindow;
+#else
+  return const_cast<ctkVTKChartView*>(this)->GetRenderWindow();
+#endif
+}
 
 // ----------------------------------------------------------------------------
 ctkVTKChartView::ctkVTKChartView(QWidget* parentWidget)