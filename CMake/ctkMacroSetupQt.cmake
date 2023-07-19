268cfdc7986ff17d3256571e7925ccce87aa547a
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2023-07-19 14:45:38 -0400

    COMP: Remove support for setting CTK_QT_VERSION to "4"

--- CMake/ctkMacroSetupQt.cmake
+++ CMake/ctkMacroSetupQt.cmake
@@ -23,10 +23,10 @@ macro(ctkMacroSetupQt)
   set(CTK_QT_VERSION "5" CACHE STRING "Expected Qt version")
   mark_as_advanced(CTK_QT_VERSION)
 
-  set_property(CACHE CTK_QT_VERSION PROPERTY STRINGS 4 5)
+  set_property(CACHE CTK_QT_VERSION PROPERTY STRINGS 5)
 
-  if(NOT (CTK_QT_VERSION VERSION_EQUAL "4" OR CTK_QT_VERSION VERSION_EQUAL "5"))
-    message(FATAL_ERROR "Expected value for CTK_QT_VERSION is either '4' or '5'")
+  if(NOT CTK_QT_VERSION VERSION_EQUAL "5")
+    message(FATAL_ERROR "Expected value for CTK_QT_VERSION is '5'")
   endif()
 
 
