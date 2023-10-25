commontk/CTK

commit a0e74a08041427d570d754c571915c849fe47618

COMP: Update CMake files removing support for Qt 4.x
* Remove use of Qt4 macros

* Anticipate integration of Qt6 by:

  - explicitly reporting an error message if corresponding CMake code is
    not implemented.

  - switching from using Qt5::Module to Qt${CTK_QT_VERSION}::Module where
    associated logic is straightforward.

CMake/ctkMacroSetupQt.cmake
@@ -29,8 +29,7 @@ macro(ctkMacroSetupQt)
     message(FATAL_ERROR "Expected value for CTK_QT_VERSION is '5'")
   endif()
 
-
-  if(CTK_QT_VERSION VERSION_GREATER "4")
+  if(CTK_QT_VERSION VERSION_EQUAL "5")
     cmake_minimum_required(VERSION 2.8.12)
     find_package(Qt5 COMPONENTS Core)
     set(CTK_QT5_COMPONENTS Core Xml XmlPatterns Concurrent Sql Test Multimedia)
@@ -57,35 +56,7 @@ macro(ctkMacroSetupQt)
     endif()
 
   else()
-    set(minimum_required_qt_version "4.6")
-
-    find_package(Qt4)
-
-    if(QT4_FOUND)
-
-      if("${QT_VERSION_MAJOR}.${QT_VERSION_MINOR}" VERSION_LESS "${minimum_required_qt_version}")
-        message(FATAL_ERROR "error: CTK requires Qt >= ${minimum_required_qt_version} -- you cannot use Qt ${QT_VERSION_MAJOR}.${QT_VERSION_MINOR}.${QT_VERSION_PATCH}.")
-      endif()
-
-      set(QT_USE_QTNETWORK ON)
-      set(QT_USE_QTSQL ON)
-      set(QT_USE_QTOPENGL ON)
-      set(QT_USE_QTXML ON)
-      set(QT_USE_QTXMLPATTERNS ON)
-      set(QT_USE_QTTEST ${BUILD_TESTING})
-      include(${QT_USE_FILE})
-
-      # Set variable QT_INSTALLED_LIBRARY_DIR that will contains
-      # Qt shared library
-      set(QT_INSTALLED_LIBRARY_DIR ${QT_LIBRARY_DIR})
-      if(WIN32)
-        get_filename_component(QT_INSTALLED_LIBRARY_DIR ${QT_QMAKE_EXECUTABLE} PATH)
-      endif()
-
-      mark_as_superbuild(QT_QMAKE_EXECUTABLE) # Qt 4
-    else()
-      message(FATAL_ERROR "error: Qt4 was not found on your system. You probably need to set the QT_QMAKE_EXECUTABLE variable")
-    endif()
+    message(FATAL_ERROR "Support for Qt${CTK_QT_VERSION} is not implemented")
   endif()
 
   mark_as_superbuild(CTK_QT_VERSION)

CMakeLists.txt
@@ -420,11 +420,6 @@ endif()
 #
 ctkMacroSetupQt()
 
-# Update CTK_BASE_LIBRARIES with QT libraries
-if(QT4_FOUND)
-  set(CTK_BASE_LIBRARIES ${CTK_BASE_LIBRARIES} ${QT_LIBRARIES} CACHE INTERNAL "CTK base libraries" FORCE)
-endif()
-
 #-----------------------------------------------------------------------------
 # To make options show up in both CTK-SuperBuild and CTK regular build, let's add them
 # before the SuperBuild script is included

Applications/Testing/Cpp/CMakeLists.txt
@@ -30,11 +30,7 @@ if(CTK_APP_ctkDICOMQuery AND CTK_APP_ctkDICOMRetrieve)
   # Add Tests
   #
   ctk_add_executable_utf8(ctkDICOMApplicationTest1 ctkDICOMApplicationTest1.cpp)
-  if(CTK_QT_VERSION VERSION_LESS "5")
-    target_link_libraries(ctkDICOMApplicationTest1 ${QT_LIBRARIES})
-  else()
-    target_link_libraries(ctkDICOMApplicationTest1 Qt5::Core)
-  endif()
+  target_link_libraries(ctkDICOMApplicationTest1 Qt${CTK_QT_VERSION}::Core)
 
   ADD_TEST( NAME ctkDICOMApplicationTest1
             COMMAND

Applications/ctkCommandLineModuleExplorer/CMakeLists.txt
@@ -49,11 +49,6 @@ set(KIT_resources
   resources/ctkCmdLineModuleExplorer.qrc
 )
 
-if(CTK_QT_VERSION VERSION_LESS "5")
-  set(QT_USE_QTUITOOLS 1)
-  include(${QT_USE_FILE})
-endif()
-
 # Target libraries - See CMake/ctkFunctionGetTargetLibraries.cmake
 # The following macro will read the target libraries from the file 'target_libraries.cmake'
 ctkFunctionGetTargetLibraries(KIT_target_libraries)

Applications/ctkExampleHost/CMakeLists.txt
@@ -29,9 +29,7 @@ set(KIT_resources
 # The following macro will read the target libraries from the file 'target_libraries.cmake'
 ctkFunctionGetTargetLibraries(KIT_target_libraries)
 
-if(CTK_QT_VERSION VERSION_GREATER "4")
-  list(APPEND KIT_target_libraries Qt5::Widgets)
-endif()
+list(APPEND KIT_target_libraries Qt${CTK_QT_VERSION}::Widgets)
 
 ctkMacroBuildApp(
   NAME ${PROJECT_NAME}

Applications/ctkPluginBrowser/CMakeLists.txt
@@ -34,9 +34,7 @@ set(KIT_resources
 # The following macro will read the target libraries from the file 'target_libraries.cmake'
 ctkFunctionGetTargetLibraries(KIT_target_libraries)
 
-if(CTK_QT_VERSION VERSION_GREATER "4")
-  list(APPEND KIT_target_libraries Qt5::Widgets)
-endif()
+list(APPEND KIT_target_libraries Qt${CTK_QT_VERSION}::Widgets)
 
 ctkMacroBuildApp(
   NAME ${PROJECT_NAME}

Libs/CommandLineModules/Core/CMakeLists.txt
@@ -79,12 +79,11 @@ set(KIT_resources
 # The following macro will read the target libraries from the file 'target_libraries.cmake'
 ctkFunctionGetTargetLibraries(KIT_target_libraries)
 
-if(CTK_QT_VERSION VERSION_GREATER "4")
-  list(APPEND KIT_target_libraries Qt5::XmlPatterns Qt5::Widgets Qt5::Concurrent)
-else()
-  set(QT_USE_QTXMLPATTERNS 1)
-  include(${QT_USE_FILE})
-endif()
+list(APPEND KIT_target_libraries
+  Qt${CTK_QT_VERSION}::Concurrent
+  Qt${CTK_QT_VERSION}::XmlPatterns
+  Qt${CTK_QT_VERSION}::Widgets
+  )
 
 ctkMacroBuildLib(
   NAME ${PROJECT_NAME}

Libs/CommandLineModules/Core/Testing/Cpp/CMakeLists.txt
@@ -25,7 +25,7 @@ include_directories(
 set(Tests_MOC_CPP)
 set(Tests_UI_CPP)
 set(Tests_RESOURCES_SRCS)
-if(CTK_QT_VERSION VERSION_GREATER "4")
+if(CTK_QT_VERSION VERSION_EQUAL "5")
   QT5_WRAP_CPP(Tests_MOC_CPP ${Tests_MOC_SRCS})
   QT5_GENERATE_MOCS(
     ctkCmdLineModuleManagerTest.cpp
@@ -36,22 +36,13 @@ if(CTK_QT_VERSION VERSION_GREATER "4")
   endif()
   QT5_ADD_RESOURCES(Tests_RESOURCES_SRCS ${Tests_RESOURCES})
 else()
-  QT4_WRAP_CPP(Tests_MOC_CPP ${Tests_MOC_SRCS})
-  QT4_GENERATE_MOCS(
-    ctkCmdLineModuleManagerTest.cpp
-    ctkCmdLineModuleXmlProgressWatcherTest.cpp
-    )
-  if(TEST_UI_FORMS)
-    QT4_WRAP_UI(Tests_UI_CPP ${Tests_UI_FORMS})
-  endif()
-  QT4_ADD_RESOURCES(Tests_RESOURCES_SRCS ${Tests_RESOURCES})
+  message(FATAL_ERROR "Support for Qt${CTK_QT_VERSION} is not implemented")
 endif()
+
 ctk_add_executable_utf8(${KIT}CppTests ${Tests} ${Tests_SRCS} ${Tests_MOC_CPP} ${Tests_UI_CPP} ${Tests_RESOURCES_SRCS})
 target_link_libraries(${KIT}CppTests ${LIBRARY_NAME} ${CTK_BASE_LIBRARIES})
 
-if(CTK_QT_VERSION VERSION_GREATER "4")
-  target_link_libraries(${KIT}CppTests Qt5::Test)
-endif()
+target_link_libraries(${KIT}CppTests Qt${CTK_QT_VERSION}::Test)
 
 #
 # Add Tests

commit aa986910786b1343101698f81fef9b612bf71e37

COMP: Fix configuration without explicitly setting CTK_QT_VERSION
Address a regression introduced in a0e74a080 (COMP: Update CMake files
removing support for Qt 4.x) by fixing the following error reported when
configuring without explicitly setting CTK_QT_VERSION:

```
cmake \
  -DQt5_DIR:PATH=$Qt5_DIR \
  -DCMAKE_BUILD_TYPE:STRING=Release \
  ../CTK

[...]
-- Checking if --no-as-needed linker flag is required - no
-- CTK_LIB_Core_WITH_BFD_SHARED is OFF
-- CTK_LIB_Core_WITH_BFD_STATIC is OFF
CMake Error at Libs/Visualization/VTK/Widgets/target_libraries.cmake:15 (message):
  Support for this Qt is not implemented
Call Stack (most recent call first):
  CMake/ctkMacroTargetLibraries.cmake:131 (include)
  CMake/ctkFunctionGenerateDGraphInput.cmake:78 (ctkFunctionCollectTargetLibraryNames)
  CMakeLists.txt:844 (ctkFunctionGenerateDGraphInput)
[...]
```

CMake/ctkMacroSetupQt.cmake
@@ -20,14 +20,6 @@
 
 #! \ingroup CMakeUtilities
 macro(ctkMacroSetupQt)
-  set(CTK_QT_VERSION "5" CACHE STRING "Expected Qt version")
-  mark_as_advanced(CTK_QT_VERSION)
-
-  set_property(CACHE CTK_QT_VERSION PROPERTY STRINGS 5)
-
-  if(NOT CTK_QT_VERSION VERSION_EQUAL "5")
-    message(FATAL_ERROR "Expected value for CTK_QT_VERSION is '5'")
-  endif()
 
   if(CTK_QT_VERSION VERSION_EQUAL "5")
     cmake_minimum_required(VERSION 2.8.12)
@@ -139,6 +131,4 @@ macro(ctkMacroSetupQt)
   else()
     message(FATAL_ERROR "Support for Qt${CTK_QT_VERSION} is not implemented")
   endif()
-
-  mark_as_superbuild(CTK_QT_VERSION)
 endmacro()

CMakeLists.txt
@@ -103,6 +103,14 @@ option(CTK_SUPERBUILD "Build ${PROJECT_NAME} and the projects it depends on." ON
 mark_as_advanced(CTK_SUPERBUILD)
 
 #-----------------------------------------------------------------------------
+# Qt version
+#
+set(CTK_QT_VERSION "5" CACHE STRING "Expected Qt version")
+mark_as_advanced(CTK_QT_VERSION)
+set_property(CACHE CTK_QT_VERSION PROPERTY STRINGS 5)
+mark_as_superbuild(CTK_QT_VERSION)
+
+#-----------------------------------------------------------------------------
 # Output directories.
 #
 foreach(type LIBRARY RUNTIME ARCHIVE)