commontk/CTK

commit 9058c638438206e4756297a9976c350aa71d7fcd

ENH: Update PythonQt backporting recent changes from MeVisLab/pythonqt
Meticulous backport of all recent changes introduced in https://github.com/MeVisLab/pythonqt/

Commit impacting only the PythonQt library are prefix with `[Backport]`
the one impacting only the generator are prefixed with `[Backport generator]`.

List of PythonQt changes:

```
$ git shortlog c4a5a155b..db525aff0 --no-merges
Christophe Giboudeaux (1):
      [Backport] Fix build with Python 3.8

David Brooks (2):
      [Backport] Use `quote` characters around includes of our headers.
      [Backport] Support Qt 5.12

Felix Petriconi (4):
      [Backport generator] Also generate python function for operator!=()
      [Backport generator] Default initialize all members
      [Backport] Replace foreach macro with range based for loop
      [Backport generator] Remove __* and _Uppercase from symbols because they must not been used

Gregor Anich (2):
      [Backport] More potential leak fixes and build fix
      [Backport] Allow setting PYTHON_VERSION and PYTHON_DIR when including python.prf, add rpath

Iakov Kirilenko (2):
      [Backport] Use XSETREF for safety
      [Backport] CI for CONFIG+=debug + UbSan for tests

James Butler (1):
      Prevent crashes during and after cleanup

Jean-Christophe Fillion-Robin (1):
      [Backport] Remove unused variable from PythonQtPrivate::callMethodInPython

John Bowler (19):
      [Backport] Qt6: initial changes to make compilation work (#114)
      [Backport generator] Replace deprecated QAtomicPointer<T>::load() (#115)
      [Backport generator] Qt6: QList<T>operator== requires const T::operator== (#116)
      [Backport generator] Qt6: qSort and qStableSort removed (#117)
      [Backport generator] Qt6: qSort, toList and fromList removal (#118)
      [Backport generator] Qt6: QString::SkipEmptyParts moved to Qt (#119)
      [Backport generator] Qt6: QTextStream compatibility (#120)
      [Backport generator] Eliminate warnings about #warning (#122)

CMakeExternals/PythonQt.cmake
@@ -82,7 +82,7 @@ if(NOT DEFINED PYTHONQT_INSTALL_DIR)
 
   ctkFunctionExtractOptimizedLibrary(PYTHON_LIBRARIES PYTHON_LIBRARY)
   if(CTK_QT_VERSION VERSION_EQUAL "5")
-    set(revision_tag c4a5a155b2942d4b003862c3317105b4a1ea6755) # patched-9
+    set(revision_tag db525aff0d8c053bddf13902107b34c93c1e3a44) # patched-9
   else()
     message(FATAL_ERROR "Support for Qt${CTK_QT_VERSION} is not implemented")
   endif()

commit 6fa9ae9d7e66e52508969137e150940e7c3e91f7

BUG: Update PythonQt and fix warnings and regression
This commit continues the effort started in 9058c638 ("ENH: Update
PythonQt backporting recent changes from MeVisLab/pythonqt", 2024-01-28)
by integrating additional upstream changes. These updates further reduce
divergence from the MeVisLab/pythonqt project.

Fixes:

* **Build warnings** when using Qt 5.15 (e.g., `QSet::unite`, `qrand()` deprecation).
* **Regression** in `setRedirectStdInCallbackEnabled` logic, introduced while backporting commontk/PythonQt@90220d92 ("Replace NULL with nullptr", 2023-01-01).
  This is resolved through the integration of commontk/PythonQt@2401816c ("BUG: Fix regression introduced in setRedirectStdInCallbackEnabled", 2025-07-02).

List of PythonQt changes:

```
$ git shortlog db525aff0..7f7105ba9 --no-merges
Fabio Loli (1):
      [Backport] Fix build with Python 3.13 (#246)

HE (1):
      [Backport] [gui] [ScriptingConsole] Fix so that history position is correct after call to setHistory so that pressing up-arrow works and shows last history item.

Iakov 'Jake' Kirilenko (3):
      [Backport] Better reference management pattern
      [Backport] Fix build with PYTHONQT_DEBUG enabled
      [Backport] PythonQtTests: Fix tests

Jean-Christophe Fillion-Robin (6):
      Fix warning related to QSet::unite when building against Qt 5.15
      Fix warning related to use of qrand() when building against Qt 5.15
      Re-introduce support for QMatrix when building against Qt < 6.0
      COMP: Replace NULL with nullptr
      COMP: Remove break statement inadvertently added
      BUG: Fix regression introduced in setRedirectStdInCallbackEnabled

MinyazevR (3):
      [Backport] Fix memory leak and add memory tests
      [Backport] Fix memory leak with PyDict_Copy
      [Backport] PythonQtTests: Change PY_VERSION_HEX for use PyConfig

Uwe Kindler (1):
      [Backport] Added MinGW build documentation for MinGW

Uwe Siems (17):
      [Backport] Handle QStringView et al as argument/return value

CMakeExternals/PythonQt.cmake
@@ -82,7 +82,7 @@ if(NOT DEFINED PYTHONQT_INSTALL_DIR)
 
   ctkFunctionExtractOptimizedLibrary(PYTHON_LIBRARIES PYTHON_LIBRARY)
   if(CTK_QT_VERSION VERSION_EQUAL "5")
-    set(revision_tag db525aff0d8c053bddf13902107b34c93c1e3a44) # patched-9
+    set(revision_tag 7f7105ba9ec59d719ded80090e049fa85ad76e79) # patched-9
   else()
     message(FATAL_ERROR "Support for Qt${CTK_QT_VERSION} is not implemented")
   endif()