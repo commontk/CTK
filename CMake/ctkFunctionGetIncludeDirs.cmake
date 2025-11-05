commontk/CTK

commit 37f5f51a699bcf014f2323c75b676e343b5c5c64

COMP: Enable AUTOUIC to simplify UI generation and prep for Qt6
Adopt target-level AUTOUIC and remove legacy UI wiring to reduce boilerplate
and align CTK with Qt6-ready CMake practices.

- Replace all `qt5_wrap_ui()` usage and drop `MY_UI_CPP`/related plumbing.
- Enable `AUTOUIC` on apps, libs, plugins, and tests.
- Set `AUTOUIC_SEARCH_PATHS` from declared UI forms (or `Resources/UI`).
- Set global `AUTOGEN_SOURCE_GROUP` to "Generated" for consistent IDE layout.
- Add AUTOGEN include dir so generated `ui_*.h` headers resolve reliably.
- Clean up: remove unused `ui_ctkThumbnailListWidget.h` include from
  `ctkDICOMThumbnailListWidget.cpp` (it was misleading `AUTOUIC`)
- Fix build: include `vtkVersionMacros.h` in ScalarsToColorsView.
- Update CommandLineModules/VTK/Widgets test CMakeLists accordingly.

Co-authored-by: Hans Johnson <hans-johnson@uiowa.edu>

CMake/ctkFunctionGetIncludeDirs.cmake
@@ -53,6 +53,21 @@ function(ctkFunctionGetIncludeDirs var_include_dirs)
              )
       endif()
 
+      list(APPEND _include_dirs
+        # Ensure generated AUTOUIC headers (ui_*.h) are discoverable.
+        #
+        # By default CMake writes them to:
+        #   <AUTOGEN_BUILD_DIR>/include
+        #
+        # where AUTOGEN_BUILD_DIR defaults to:
+        #   <target-binary-dir>/<target-name>_autogen
+        #
+        # References:
+        # - https://cmake.org/cmake/help/latest/manual/cmake-qt.7.html#autouic
+        # - https://cmake.org/cmake/help/latest/prop_tgt/AUTOGEN_BUILD_DIR.html
+        ${${dep}_BINARY_DIR}/${dep}_autogen/include
+        )
+
       # For external projects, CTKConfig.cmake contains variables
       # listening the include dirs for CTK libraries and plugins
       if(${dep}_INCLUDE_DIRS)

CMake/ctkMacroBuildApp.cmake
@@ -71,7 +71,6 @@ macro(ctkMacroBuildApp)
     )
 
   # Make sure variable are cleared
-  set(MY_UI_CPP)
   set(MY_MOC_CPP)
   set(MY_QRC_SRCS)
 
@@ -84,7 +83,6 @@ macro(ctkMacroBuildApp)
         QT5_WRAP_CPP(MY_MOC_CPP ${moc_src} OPTIONS -f${moc_src} OPTIONS -DHAVE_QT5)
       endforeach()
     endif()
-    QT5_WRAP_UI(MY_UI_CPP ${MY_UI_FORMS})
     if(DEFINED MY_RESOURCES)
       QT5_ADD_RESOURCES(MY_QRC_SRCS ${MY_RESOURCES})
     endif()
@@ -100,15 +98,29 @@ macro(ctkMacroBuildApp)
   source_group("Generated" FILES
     ${MY_QRC_SRCS}
     ${MY_MOC_CPP}
-    ${MY_UI_CPP}
     )
 
   # Create executable
   ctk_add_executable_utf8(${proj_name}
     ${MY_SRCS}
     ${MY_MOC_CPP}
-    ${MY_UI_CPP}
     ${MY_QRC_SRCS}
+    )
+
+  # Configure CMake Qt automatic code generation
+  set(uic_search_paths)
+  foreach(ui_src IN LISTS MY_UI_FORMS)
+    if(NOT IS_ABSOLUTE ${ui_src})
+      set(ui_src "${CMAKE_CURRENT_SOURCE_DIR}/${ui_src}")
+    endif()
+    get_filename_component(ui_path ${ui_src} PATH)
+    list(APPEND uic_search_paths ${ui_path})
+  endforeach()
+  list(REMOVE_DUPLICATES uic_search_paths)
+
+  set_target_properties(${proj_name} PROPERTIES
+    AUTOUIC ON
+    AUTOUIC_SEARCH_PATHS "${uic_search_paths}"
     )
 
   # Set labels associated with the target.

CMake/ctkMacroBuildLib.cmake
@@ -63,8 +63,6 @@ macro(ctkMacroBuildLib)
   set(my_includes
     ${CMAKE_CURRENT_SOURCE_DIR}
     ${CMAKE_CURRENT_BINARY_DIR}
-    # with CMake >2.9, use QT4_MAKE_OUTPUT_FILE instead ?
-    ${CMAKE_CURRENT_BINARY_DIR}/Resources/UI
     )
 
   # Add the include directories from the library dependencies
@@ -105,7 +103,6 @@ ${${MY_EXPORT_CUSTOM_CONTENT_FROM_VARIABLE}}
 
   # Make sure variable are cleared
   set(MY_MOC_CPP)
-  set(MY_UI_CPP)
   set(MY_QRC_SRCS)
 
   # Wrap
@@ -122,15 +119,6 @@ ${${MY_EXPORT_CUSTOM_CONTENT_FROM_VARIABLE}}
   endif()
   if(MY_GENERATE_MOC_SRCS)
     QT5_GENERATE_MOCS(${MY_GENERATE_MOC_SRCS} USE_MOC_EXTENSION)
-  endif()
-  if(CTK_QT_VERSION VERSION_EQUAL "5")
-    if(Qt5Widgets_FOUND)
-      qt5_wrap_ui(MY_UI_CPP ${MY_UI_FORMS})
-    elseif(MY_UI_FORMS)
-      message(WARNING "Argument UI_FORMS ignored because Qt5Widgets module was not specified")
-    endif()
-  else()
-    message(FATAL_ERROR "Support for Qt${CTK_QT_VERSION} is not implemented")
   endif()
 
   if(DEFINED MY_RESOURCES AND NOT MY_RESOURCES STREQUAL "")
@@ -149,15 +137,19 @@ ${${MY_EXPORT_CUSTOM_CONTENT_FROM_VARIABLE}}
   source_group("Generated" FILES
     ${MY_QRC_SRCS}
     ${MY_MOC_CPP}
-    ${MY_UI_CPP}
     ${MOC_CPP_DECORATOR}
     )
 
   add_library(${lib_name} ${MY_LIBRARY_TYPE}
     ${MY_SRCS}
     ${MY_MOC_CPP}
-    ${MY_UI_CPP}
     ${MY_QRC_SRCS}
+    )
+
+  # Configure CMake Qt automatic code generation
+  set_target_properties(${lib_name} PROPERTIES
+    AUTOUIC ON
+    AUTOUIC_SEARCH_PATHS "${CMAKE_CURRENT_SOURCE_DIR}/Resources/UI"
     )
 
   # Set labels associated with the target.

CMake/ctkMacroBuildPlugin.cmake
@@ -162,7 +162,6 @@ macro(ctkMacroBuildPlugin)
 
   # Make sure variable are cleared
   set(MY_MOC_CPP)
-  set(MY_UI_CPP)
   set(MY_QRC_SRCS)
 
   # Wrap
@@ -178,7 +177,6 @@ macro(ctkMacroBuildPlugin)
         QT5_WRAP_CPP(MY_MOC_CPP ${moc_src} OPTIONS -f${moc_src} -DHAVE_QT5 ${MY_MOC_OPTIONS} ${target})
       endforeach()
     endif()
-    QT5_WRAP_UI(MY_UI_CPP ${MY_UI_FORMS})
     if(DEFINED MY_RESOURCES)
       QT5_ADD_RESOURCES(MY_QRC_SRCS ${MY_RESOURCES})
     endif()
@@ -265,14 +263,12 @@ macro(ctkMacroBuildPlugin)
   source_group("Generated" FILES
     ${MY_QRC_SRCS}
     ${MY_MOC_CPP}
-    ${MY_UI_CPP}
     ${_plugin_qm_files}
     )
 
   add_library(${lib_name} ${MY_LIBRARY_TYPE}
     ${MY_SRCS}
     ${MY_MOC_CPP}
-    ${MY_UI_CPP}
     ${MY_QRC_SRCS}
     ${_plugin_qm_files}
     )
@@ -280,6 +276,22 @@ macro(ctkMacroBuildPlugin)
   target_include_directories(${lib_name}
     PUBLIC "$<BUILD_INTERFACE:${my_includes}>"
            "$<INSTALL_INTERFACE:${CTK_INSTALL_PLUGIN_INCLUDE_DIR}/${Plugin-SymbolicName}>"
+    )
+
+  # Configure CMake Qt automatic code generation
+  set(uic_search_paths)
+  foreach(ui_src IN LISTS MY_UI_FORMS)
+    if(NOT IS_ABSOLUTE ${ui_src})
+      set(ui_src "${CMAKE_CURRENT_SOURCE_DIR}/${ui_src}")
+    endif()
+    get_filename_component(ui_path ${ui_src} PATH)
+    list(APPEND uic_search_paths ${ui_path})
+  endforeach()
+  list(REMOVE_DUPLICATES uic_search_paths)
+
+  set_target_properties(${lib_name} PROPERTIES
+    AUTOUIC ON
+    AUTOUIC_SEARCH_PATHS "${uic_search_paths}"
     )
 
   if(MY_TEST_PLUGIN)

CMake/ctkMacroBuildQtPlugin.cmake
@@ -82,7 +82,6 @@ macro(ctkMacroBuildQtPlugin)
 
   # Make sure variable are cleared
   set(MY_MOC_CPP)
-  set(MY_UI_CPP)
   set(MY_QRC_SRCS)
 
   # Wrap
@@ -97,12 +96,6 @@ macro(ctkMacroBuildQtPlugin)
     if(DEFINED MY_RESOURCES)
       qt5_add_resources(MY_QRC_SRCS ${MY_RESOURCES})
     endif()
-
-    if(Qt5Widgets_FOUND)
-      qt5_wrap_ui(MY_UI_CPP ${MY_UI_FORMS})
-    elseif(MY_UI_FORMS)
-      message(WARNING "Argument UI_FORMS ignored because Qt5Widgets module was not specified")
-    endif()
   else()
     message(FATAL_ERROR "Support for Qt${CTK_QT_VERSION} is not implemented")
   endif()
@@ -115,14 +108,28 @@ macro(ctkMacroBuildQtPlugin)
   source_group("Generated" FILES
     ${MY_MOC_CPP}
     ${MY_QRC_SRCS}
-    ${MY_UI_CPP}
     )
 
   add_library(${lib_name} ${MY_LIBRARY_TYPE}
     ${MY_SRCS}
     ${MY_MOC_CPP}
-    ${MY_UI_CPP}
     ${MY_QRC_SRCS}
+    )
+
+  # Configure CMake Qt automatic code generation
+  set(uic_search_paths)
+  foreach(ui_src IN LISTS MY_UI_FORMS)
+    if(NOT IS_ABSOLUTE ${ui_src})
+      set(ui_src "${CMAKE_CURRENT_SOURCE_DIR}/${ui_src}")
+    endif()
+    get_filename_component(ui_path ${ui_src} PATH)
+    list(APPEND uic_search_paths ${ui_path})
+  endforeach()
+  list(REMOVE_DUPLICATES uic_search_paths)
+
+  set_target_properties(${lib_name} PROPERTIES
+    AUTOUIC ON
+    AUTOUIC_SEARCH_PATHS "${uic_search_paths}"
     )
 
   # Extract library name associated with the plugin and use it as label

CMakeLists.txt
@@ -975,6 +975,8 @@ endif()
 #
 ctkMacroSetupQt()
 
+set_property(GLOBAL PROPERTY AUTOGEN_SOURCE_GROUP "Generated")
+
 #-----------------------------------------------------------------------------
 include(CMake/ctkBlockCheckDependencies.cmake)
 

Libs/CommandLineModules/Core/Testing/Cpp/CMakeLists.txt
@@ -23,7 +23,6 @@ include_directories(
   )
 
 set(Tests_MOC_CPP)
-set(Tests_UI_CPP)
 set(Tests_RESOURCES_SRCS)
 if(CTK_QT_VERSION VERSION_EQUAL "5")
   QT5_WRAP_CPP(Tests_MOC_CPP ${Tests_MOC_SRCS})
@@ -32,15 +31,12 @@ if(CTK_QT_VERSION VERSION_EQUAL "5")
     ctkCmdLineModuleXmlProgressWatcherTest.cpp
     USE_MOC_EXTENSION
     )
-  if(TEST_UI_FORMS)
-    QT5_WRAP_UI(Tests_UI_CPP ${Tests_UI_FORMS})
-  endif()
   QT5_ADD_RESOURCES(Tests_RESOURCES_SRCS ${Tests_RESOURCES})
 else()
   message(FATAL_ERROR "Support for Qt${CTK_QT_VERSION} is not implemented")
 endif()
 
-ctk_add_executable_utf8(${KIT}CppTests ${Tests} ${Tests_SRCS} ${Tests_MOC_CPP} ${Tests_UI_CPP} ${Tests_RESOURCES_SRCS})
+ctk_add_executable_utf8(${KIT}CppTests ${Tests} ${Tests_SRCS} ${Tests_MOC_CPP} ${Tests_RESOURCES_SRCS})
 target_link_libraries(${KIT}CppTests ${LIBRARY_NAME} ${CTK_BASE_LIBRARIES})
 
 target_link_libraries(${KIT}CppTests Qt${CTK_QT_VERSION}::Test)

Libs/CommandLineModules/Frontend/QtGui/Testing/Cpp/CMakeLists.txt
@@ -25,7 +25,6 @@ include_directories(
   )
 
 set(Tests_MOC_CPP)
-set(Tests_UI_CPP)
 set(Tests_RESOURCES_SRCS)
 if(CTK_QT_VERSION VERSION_EQUAL "5")
   QT5_WRAP_CPP(Tests_MOC_CPP ${Tests_MOC_SRCS})
@@ -34,15 +33,12 @@ if(CTK_QT_VERSION VERSION_EQUAL "5")
     ctkCmdLineModuleQtXslTransformTest.cpp
     USE_MOC_EXTENSION
     )
-  if(TEST_UI_FORMS)
-    QT5_WRAP_UI(Tests_UI_CPP ${Tests_UI_FORMS})
-  endif()
   QT5_ADD_RESOURCES(Tests_RESOURCES_SRCS ${Tests_RESOURCES})
 else()
   message(FATAL_ERROR "Support for Qt${CTK_QT_VERSION} is not implemented")
 endif()
 
-ctk_add_executable_utf8(${KIT}CppTests ${Tests} ${Tests_SRCS} ${Tests_MOC_CPP} ${Tests_UI_CPP} ${Tests_RESOURCES_SRCS})
+ctk_add_executable_utf8(${KIT}CppTests ${Tests} ${Tests_SRCS} ${Tests_MOC_CPP} ${Tests_RESOURCES_SRCS})
 target_link_libraries(${KIT}CppTests ${LIBRARY_NAME} ${CTK_BASE_LIBRARIES})
 
 target_link_libraries(${KIT}CppTests Qt${CTK_QT_VERSION}::Test)

commit 162cf94ac0e61dee64b7b31e468b9dfb60ffb4e5

COMP: Fix AUTOUIC include dir for multi-config generators
Add the autogen include path with the per-config suffix when
GENERATOR_IS_MULTI_CONFIG is true:
  <binary>/<target>_autogen/include_$<CONFIG>

This ensures ui_*.h headers are found under VS/Xcode/Ninja
Multi-Config, while preserving the single-config layout:
  <binary>/<target>_autogen/include

It fixes regression introduced in 37f5f51a ("COMP: Enable AUTOUIC to simplify
UI generation and prep for Qt6", 2025-11-02)

CMake/ctkFunctionGetIncludeDirs.cmake
@@ -26,6 +26,8 @@ function(ctkFunctionGetIncludeDirs var_include_dirs)
     message(FATAL_ERROR "No targets given")
   endif()
 
+  get_property(_isMultiConfig GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
+
   set(_include_dirs ${${var_include_dirs}} ${CTK_CONFIG_H_INCLUDE_DIR})
   foreach(_target ${ARGN})
 
@@ -54,10 +56,15 @@ function(ctkFunctionGetIncludeDirs var_include_dirs)
       endif()
 
       list(APPEND _include_dirs
-        # Ensure generated AUTOUIC headers (ui_*.h) are discoverable.
+        # Ensure AUTOUIC-generated headers (ui_*.h) are on the include path.
         #
         # By default CMake writes them to:
-        #   <AUTOGEN_BUILD_DIR>/include
+        #
+        #   - Single-config generators (Ninja/Makefiles):
+        #       <AUTOGEN_BUILD_DIR>/include
+        #
+        #   - Multi-config generators (VS, Xcode, Ninja Multi-Config):
+        #       <AUTOGEN_BUILD_DIR>/include_<CONFIG>
         #
         # where AUTOGEN_BUILD_DIR defaults to:
         #   <target-binary-dir>/<target-name>_autogen
@@ -65,7 +72,7 @@ function(ctkFunctionGetIncludeDirs var_include_dirs)
         # References:
         # - https://cmake.org/cmake/help/latest/manual/cmake-qt.7.html#autouic
         # - https://cmake.org/cmake/help/latest/prop_tgt/AUTOGEN_BUILD_DIR.html
-        ${${dep}_BINARY_DIR}/${dep}_autogen/include
+        ${${dep}_BINARY_DIR}/${dep}_autogen/include$<$<BOOL:${_isMultiConfig}>:_$<CONFIG>>
         )
 
       # For external projects, CTKConfig.cmake contains variables